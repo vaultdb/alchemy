82f5bfc95a65ef03a7b1ff3556d8c7b5386db7d8
johesbater@gmail.com <johesbater@gmail.com>
2019-02-12 15:05:46 -0600

    exec script addition

--- src/main/java/org/smcql/executor/EMPQueryExecutor.java
+++ src/main/java/org/smcql/executor/EMPQueryExecutor.java
@@ -2,14 +2,12 @@ package org.smcql.executor;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
-import org.apache.commons.exec.ExecuteException;
 import org.apache.commons.exec.PumpStreamHandler;
 import org.apache.commons.lang3.StringUtils;
 import org.gridkit.nanocloud.Cloud;
