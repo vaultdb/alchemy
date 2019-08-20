7dfe75497a414dc5944609132d129cfc30ad8bb4
Madhav Suresh <madhav@thoughtbison.net>
2019-08-20 16:17:19 -0500

    fix aliasing issue with TPC-H 18
    	* Query 18 in TPC-H was failing because the l_orderkey is
    	  aliased in the final join of the query. This is because
    	  lineitem is scanned twice.
    	* This commit adds two new fields to SecureRelDataTypeField
    	  in order to keep track of whether the field contains an
              aliased value. Aliasing of attributes happens in calcite:
    	  org.apache.calcite.sql.validate.SqlValidatorUtil.java:557
    	* For joins, when determining the execution mode, the
     	  planner uses the unaliased attribute name when applicable.

--- src/main/java/org/vaultdb/type/SecureRelDataTypeField.java
+++ src/main/java/org/vaultdb/type/SecureRelDataTypeField.java
@@ -18,204 +18,215 @@ import org.vaultdb.db.schema.statistics.ObliviousFieldStatistics;
 
 // thin wrapper on top of RelDataTypeField for attaching security policy to an attribute
 // how do we call this at schema construction time?
-// create a secure table 
+// create a secure table
 // instantiate SecureTable where we do "Table table = schema.getTable(tableName);" now
 public class SecureRelDataTypeField extends RelDataTypeFieldImpl implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+  /** */
+  private static final long serialVersionUID = 1L;
 
-	public enum SecurityPolicy {
-		Public, Protected, Private
-	};
+  public enum SecurityPolicy {
+    Public,
+    Protected,
+    Private
+  };
 
-	SecurityPolicy policy = SecurityPolicy.Private;
-	RelDataTypeField baseField;
-	ObliviousFieldStatistics statistics;
-	
-	// both of below are null if field sourced from greater than one attribute
-	// stored in original tables
-	private String storedTable;
-	private String storedAttribute;
-	transient List<LogicalFilter> filters;
+  SecurityPolicy policy = SecurityPolicy.Private;
+  RelDataTypeField baseField;
+  ObliviousFieldStatistics statistics;
+  private boolean aliased;
+  private String unaliasedName;
 
-	public SecureRelDataTypeField(String name, int index, RelDataType type)  {
-		super(name, index, type);
-		filters = new ArrayList<LogicalFilter>();
-		//statistics = new ObliviousFieldStatistics(this); 
-	}
+  // both of below are null if field sourced from greater than one attribute
+  // stored in original tables
+  private String storedTable;
+  private String storedAttribute;
+  transient List<LogicalFilter> filters;
 
-	public  SecureRelDataTypeField(RelDataTypeField baseField, SecurityPolicy secPolicy, String aStoredTable)  {
-		super(baseField.getName(), baseField.getIndex(), baseField.getType());
-		this.baseField = baseField;
-		policy = secPolicy;
-		filters = new ArrayList<LogicalFilter>();
-		storedTable = aStoredTable;
-		//statistics = new ObliviousFieldStatistics(this); 
-	}
+  public SecureRelDataTypeField(String name, int index, RelDataType type) {
+    super(name, index, type);
+    filters = new ArrayList<LogicalFilter>();
+    // statistics = new ObliviousFieldStatistics(this);
+  }
 
-	public  SecureRelDataTypeField(RelDataTypeField baseField, SecurityPolicy secPolicy, ObliviousFieldStatistics theStats) {
-		super(baseField.getName(), baseField.getIndex(), baseField.getType());
-		this.baseField = baseField;
-		policy = secPolicy;
-		filters = new ArrayList<LogicalFilter>();
-		statistics = theStats;
-	}
+  public SecureRelDataTypeField(
+      RelDataTypeField baseField, SecurityPolicy secPolicy, String aStoredTable) {
+    super(baseField.getName(), baseField.getIndex(), baseField.getType());
+    this.baseField = baseField;
+    policy = secPolicy;
+    filters = new ArrayList<LogicalFilter>();
+    storedTable = aStoredTable;
+    // statistics = new ObliviousFieldStatistics(this);
+  }
 
-	
-	public SecureRelDataTypeField(RelDataTypeField baseField, SecurityPolicy secPolicy, String aStoredTable,
-			String aStoredAttribute, LogicalFilter aFilter) throws Exception {
-		super(baseField.getName(), baseField.getIndex(), baseField.getType());
-		this.baseField = baseField;
-		policy = secPolicy;
-		setStoredTable(aStoredTable);
-		setStoredAttribute(aStoredAttribute);
-		filters = new ArrayList<LogicalFilter>();
-		addFilter(aFilter);
-		//statistics = new ObliviousFieldStatistics(this); 
-		
-	}
+  public SecureRelDataTypeField(
+      RelDataTypeField baseField, SecurityPolicy secPolicy, ObliviousFieldStatistics theStats) {
+    super(baseField.getName(), baseField.getIndex(), baseField.getType());
+    this.baseField = baseField;
+    policy = secPolicy;
+    filters = new ArrayList<LogicalFilter>();
+    statistics = theStats;
+  }
 
-	// quasi-copy constructor
-	public SecureRelDataTypeField(RelDataTypeField aBaseField, SecureRelDataTypeField src) {
-		super(aBaseField.getName(), src.getBaseField().getIndex(), src.getBaseField().getType());
-		baseField = aBaseField;
-		policy = src.getSecurityPolicy();
-		storedTable = src.getStoredTable();
-		storedAttribute = src.getStoredAttribute();
-		filters = new ArrayList<LogicalFilter>(src.getFilters());
-		statistics = src.statistics;
-	}
+  public SecureRelDataTypeField(
+      RelDataTypeField baseField,
+      SecurityPolicy secPolicy,
+      String aStoredTable,
+      String aStoredAttribute,
+      LogicalFilter aFilter)
+      throws Exception {
+    super(baseField.getName(), baseField.getIndex(), baseField.getType());
+    this.baseField = baseField;
+    policy = secPolicy;
+    setStoredTable(aStoredTable);
+    setStoredAttribute(aStoredAttribute);
+    filters = new ArrayList<LogicalFilter>();
+    addFilter(aFilter);
+    // statistics = new ObliviousFieldStatistics(this);
 
-	private void writeObject(ObjectOutputStream out) throws IOException {
-		List<String> field = new ArrayList<String>();
-		field.add(Integer.toString(baseField.getIndex()));
-		field.add(baseField.getName());
-		field.add(Integer.toString(baseField.getType().getPrecision()));
-		field.add(Integer.toString(baseField.getType().getScale()));
+  }
 
-		out.writeObject(field);
-		out.writeObject(baseField.getType().getSqlTypeName());
-		out.writeObject(storedTable);
-		out.writeObject(storedAttribute);
-		out.writeObject(policy);
-	}
+  // quasi-copy constructor
+  public SecureRelDataTypeField(RelDataTypeField aBaseField, SecureRelDataTypeField src) {
+    super(aBaseField.getName(), src.getBaseField().getIndex(), src.getBaseField().getType());
+    if (!src.getName().equals(aBaseField.getName())) {
+      aliased = true;
+    }
+    unaliasedName = src.getName();
+    baseField = aBaseField;
+    policy = src.getSecurityPolicy();
+    storedTable = src.getStoredTable();
+    storedAttribute = src.getStoredAttribute();
+    filters = new ArrayList<LogicalFilter>(src.getFilters());
+    statistics = src.statistics;
+  }
 
-	@SuppressWarnings("unchecked")
-	private void readObject(ObjectInputStream ois) throws ClassNotFoundException, IOException {
-		List<String> field = (List<String>) ois.readObject();
-		SqlTypeName stn = (SqlTypeName) ois.readObject();
+  private void writeObject(ObjectOutputStream out) throws IOException {
+    List<String> field = new ArrayList<String>();
+    field.add(Integer.toString(baseField.getIndex()));
+    field.add(baseField.getName());
+    field.add(Integer.toString(baseField.getType().getPrecision()));
+    field.add(Integer.toString(baseField.getType().getScale()));
 
-		int index = Integer.parseInt(field.get(0));
-		String name = field.get(1);
-		int precision = Integer.parseInt(field.get(2));
-		int scale = Integer.parseInt(field.get(3));
+    out.writeObject(field);
+    out.writeObject(baseField.getType().getSqlTypeName());
+    out.writeObject(storedTable);
+    out.writeObject(storedAttribute);
+    out.writeObject(policy);
+  }
 
-		SqlTypeFactoryImpl factory = new SqlTypeFactoryImpl(RelDataTypeSystem.DEFAULT);
-		baseField = new RelDataTypeFieldImpl(name, index, factory.createSqlType(stn, precision, scale));
-		storedTable = (String)ois.readObject();
-		storedAttribute = (String)ois.readObject();
-		policy = (SecurityPolicy)ois.readObject();
-	}
+  @SuppressWarnings("unchecked")
+  private void readObject(ObjectInputStream ois) throws ClassNotFoundException, IOException {
+    List<String> field = (List<String>) ois.readObject();
+    SqlTypeName stn = (SqlTypeName) ois.readObject();
 
-	public int size() {
-		TypeMap tMap = TypeMap.getInstance();
-		return tMap.sizeof(this);
-	}
+    int index = Integer.parseInt(field.get(0));
+    String name = field.get(1);
+    int precision = Integer.parseInt(field.get(2));
+    int scale = Integer.parseInt(field.get(3));
 
-	// for table scans
-	public void initializeStatistics() {
-		statistics = new ObliviousFieldStatistics(this);
-	}
-	
-	
-	public void initializeStatistics(ObliviousFieldStatistics stats) {
-		statistics = new ObliviousFieldStatistics(this, stats);
-	}
-	
-	@Override
-	public int getIndex() {
-		if (baseField == null)
-			return super.getIndex();
-		return baseField.getIndex();
-	}
+    SqlTypeFactoryImpl factory = new SqlTypeFactoryImpl(RelDataTypeSystem.DEFAULT);
+    baseField = new RelDataTypeFieldImpl(name, index, factory.createSqlType(stn, precision, scale));
+    storedTable = (String) ois.readObject();
+    storedAttribute = (String) ois.readObject();
+    policy = (SecurityPolicy) ois.readObject();
+  }
 
-	
-	@Override 
-	public boolean equals(Object obj) {
-		if(obj == this) {
-			return true;
-		}
-		
-		if(!(obj instanceof SecureRelDataTypeField)) {
-			return ((RelDataTypeFieldImpl) this).equals(obj); // default to superclass
-		}
-		
-		SecureRelDataTypeField that = (SecureRelDataTypeField) obj;
-		if(this.getIndex() == that.getIndex()
-				&& this.getName().equals(that.getName())
-				&& this.getType().equals(that.getType()))
-			return true;
-		
-		return false;
-		
-	}
-	
-	public SecurityPolicy getSecurityPolicy() {
-		return policy;
-	}
+  public int size() {
+    TypeMap tMap = TypeMap.getInstance();
+    return tMap.sizeof(this);
+  }
 
-	public void setSecurityPolicy(SecurityPolicy policy) {
-		this.policy = policy;
-	}
+  // for table scans
+  public void initializeStatistics() {
+    statistics = new ObliviousFieldStatistics(this);
+  }
 
-	public RelDataTypeField getBaseField() {
-		return baseField;
-	}
+  public void initializeStatistics(ObliviousFieldStatistics stats) {
+    statistics = new ObliviousFieldStatistics(this, stats);
+  }
 
-	@Override
-	public String toString() {
-		return baseField + " " + policy;
-	}
+  @Override
+  public int getIndex() {
+    if (baseField == null) return super.getIndex();
+    return baseField.getIndex();
+  }
 
-	public String getStoredTable() {
-		return storedTable;
-	}
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
+    }
 
-	public void setStoredTable(String storedTable) {
-		this.storedTable = storedTable;
-	}
+    if (!(obj instanceof SecureRelDataTypeField)) {
+      return ((RelDataTypeFieldImpl) this).equals(obj); // default to superclass
+    }
 
-	public String getStoredAttribute() {
-		return storedAttribute;
-	}
-	
-	public ObliviousFieldStatistics getStatistics() {
-		return statistics;
-	}
+    SecureRelDataTypeField that = (SecureRelDataTypeField) obj;
+    if (this.getIndex() == that.getIndex()
+        && this.getName().equals(that.getName())
+        && this.getType().equals(that.getType())) return true;
 
-	public void setStoredAttribute(String storedAttribute) {
-		this.storedAttribute = storedAttribute;
-	}
+    return false;
+  }
 
-	// for testing equality of slice keys
-	public void addFilter(LogicalFilter aFilter) {
-		if (aFilter != null)
-			filters.add(aFilter);
-	}
+  public SecurityPolicy getSecurityPolicy() {
+    return policy;
+  }
 
-	public List<LogicalFilter> getFilters() {
-		return filters;
-	}
+  public void setSecurityPolicy(SecurityPolicy policy) {
+    this.policy = policy;
+  }
 
-	public boolean isSliceAble() {
-		if (storedAttribute != null && storedTable != null && policy == SecurityPolicy.Public)
-			return true;
-		return false;
-	}
-	
-	
-	
+  public RelDataTypeField getBaseField() {
+    return baseField;
+  }
+
+  @Override
+  public String toString() {
+    return baseField + " " + policy;
+  }
+
+  public String getStoredTable() {
+    return storedTable;
+  }
+
+  public void setStoredTable(String storedTable) {
+    this.storedTable = storedTable;
+  }
+
+  public String getStoredAttribute() {
+    return storedAttribute;
+  }
+
+  public ObliviousFieldStatistics getStatistics() {
+    return statistics;
+  }
+
+  public void setStoredAttribute(String storedAttribute) {
+    this.storedAttribute = storedAttribute;
+  }
+
+  // for testing equality of slice keys
+  public void addFilter(LogicalFilter aFilter) {
+    if (aFilter != null) filters.add(aFilter);
+  }
+
+  public boolean isAliased() {
+    return aliased;
+  }
+
+  public String getUnaliasedName() {
+    return unaliasedName;
+  }
+
+  public List<LogicalFilter> getFilters() {
+    return filters;
+  }
+
+  public boolean isSliceAble() {
+    if (storedAttribute != null && storedTable != null && policy == SecurityPolicy.Public)
+      return true;
+    return false;
+  }
 }
