5a30e473a0187b2c3c3b48a2e98f9993ab746d8a
Madhav Suresh <madhav@thoughtbison.net>
2020-02-05 13:40:39 -0600

    basic histogram template
    	* This commit introduces a basic Histogram utility
    	* TODO: still needs testing
    	* From the research goal/meeting notes:
    		- Privacy budget is infinite
    	        - All histograms
    		- Selection: predicate cols
    		- Join: join predicate
    		- Aggregate: group by cols

--- src/main/java/org/vaultdb/util/Histograms.java
+++ src/main/java/org/vaultdb/util/Histograms.java
@@ -1,4 +1,20 @@
 package org.vaultdb.util;
 
+import org.vaultdb.db.data.QueryTable;
+import org.vaultdb.executor.plaintext.SqlQueryExecutor;
+
+import java.util.ArrayList;
+
 public class Histograms {
+
+  // TODO(@cfgong) Run a histogram query with equal width bins. This requires
+  public ArrayList<QueryTable> getHistogram(
+      String table_name, ArrayList<String> column_names, int num_bins) throws Exception {
+
+    // TODO(@cfgong) example of how to query using SqlQueryExecutor
+    // https://github.com/smcql/smcql/blob/master/src/main/java/org/smcql/plan/execution/slice/statistics/StatisticsCollector.java
+    QueryTable t = SqlQueryExecutor.query("SELECT l_orderkey FROM lineitem LIMIT 10");
+
+    return null;
+  }
 }
