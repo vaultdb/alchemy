dbd3f03360f9ea121473b7f5fd4b9320ed8323cd
shashank-satish-d <shashank-satish-d@u.northwestern.edu>
2020-02-26 18:44:18 -0600

    removed InitDummy in aggregate, cleanup
      * #4f11 does Dummy initialization during ingest
      * various code cleanup

--- src/main/cpp/operators/secure_aggregate.cpp
+++ src/main/cpp/operators/secure_aggregate.cpp
@@ -3,8 +3,8 @@
 //
 
 #include "secure_aggregate.h"
-#include <querytable/query_table.h>
 #include <common/macros.h>
+#include <querytable/query_table.h>
 std::unique_ptr<QueryTable> Aggregate(QueryTable *input,
                                       const AggregateDef &def) {
   // TODO(madhavsuresh): for tpc-h 1 need to implement SUM, AVG, and COUNT
@@ -19,26 +19,14 @@ std::unique_ptr<QueryTable> Aggregate(QueryTable *input,
     sum_vector.emplace_back(64, 0, emp::BOB);
   }
   emp::Bit *isDummy = new emp::Bit(false, emp::PUBLIC);
-  // emp::Integer *value = new emp::Integer
-  // int count_star = input->GetNumTuples();
 
   emp::Integer count_emp(64, 0, emp::BOB);
 
-  // std::cout<<"Count(*) = "<<count_star;
-  // if(count_emp){std::cout<<"There's our size for count"<<count_emp->size();}
-
   emp::Integer one(64, 1, emp::PUBLIC);
   emp::Integer zero(64, 1, emp::PUBLIC);
-  // types::Value isDummy;
-  // emp::Integer value;
   for (int row = 0; row < input->GetNumTuples(); row++) {
-    //    *sum = *sum +
-    //        emp::If(*input->GetTuple(row)->InitDummy())
-    input->GetTuple(row)->InitDummy();
     *isDummy = *input->GetTuple(row)->GetDummyFlag()->GetEmpBit();
     count_emp = count_emp + emp::If(*isDummy, zero, one);
-    //*isDummy =
-    //*input->GetTuple(row)->GetField(def.index)->GetValue()->GetEmpBit();
     for (int idx = 0; idx < def.index.size(); idx++) {
       sum_vector[idx] =
           sum_vector[idx] + emp::If(*isDummy, zero,
@@ -46,40 +34,16 @@ std::unique_ptr<QueryTable> Aggregate(QueryTable *input,
                                          ->GetField(def.index[idx])
                                          ->GetValue()
                                          ->GetEmpInt());
-
-
-      std::cout << input->GetTuple(row)
-                       ->GetField(def.index[idx])
-                       ->GetValue()
-                       ->GetEmpInt()
-                       ->reveal<int64_t>(emp::PUBLIC)
-                << "|\t";
     }
-
-
-    // std::cout<<"\n=========== count ============
-    // "<<average->reveal<int64_t>(emp::PUBLIC); *sum emp::Float::operator/
-    //*count_emp;
-
-    // std::cout<<"\n=========== count ===========
-    // "<<count_emp->reveal<int64_t>(emp::PUBLIC); emp::Float f =
-    // emp::Float(emp::Float());
   }
-  std::cout << std::endl;
-  VAULTDB_DEBUG(count_emp, count_emp.reveal<int64_t>(emp::PUBLIC));
   std::vector<emp::Integer> average_vector;
   for (int i = 0; i < def.index.size(); i++) {
     average_vector.emplace_back(64, 0, emp::BOB);
-    average_vector[i] = (sum_vector[i])/ count_emp;
+    average_vector[i] = (sum_vector[i]) / count_emp;
     const QueryField f(average_vector[i], average_vector[i].length, i);
     aggregate_output->GetTuple(0)->PutField(i, &f);
   }
 
-  // int table_sum = sum->reveal<int64_t>(emp::PUBLIC);
-  // std::cout << "\nOutput of sum " << table_sum;
-  // std::cout << "\nAverage of the table: "<< emp::Float(table_sum/count_star);
-
   // TODO: implement this for float as well (returning int(AVG) for now
-  std::cout << "**************" << std::endl;
   return aggregate_output;
 }
