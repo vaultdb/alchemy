967e71915d5b5ab6a9a93d0e0f5ffd69fdd7e8d2
Madhav Suresh <madhav@thoughtbison.net>
2020-03-04 16:19:10 -0600

    shell for StatisticsCatalog
    * This introduces a shell for the StatisticsCatalog with a simple
    API

---
+++ src/main/java/org/vaultdb/util/StatisticsCatalog.java
@@ -0,0 +1,16 @@
+package org.vaultdb.util;
+
+import org.vaultdb.db.data.QueryTable;
+
+import java.util.List;
+
+public class StatisticsCatalog {
+
+  static QueryTable getCheckConstraints(String tableName, String columnName) {
+    return null;
+  }
+
+  static QueryTable getHistogram(String tableName, String columnName) {
+    return null;
+  }
+}
