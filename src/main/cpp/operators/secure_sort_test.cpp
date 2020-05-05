bce00f74098ccf4729842d967d54872b2bcb465e
Salome Kariuki <salome@Salomes-MacBook-Pro.local>
2020-05-05 16:34:35 -0500

    added encrypted fields to test

--- src/main/cpp/operators/secure_sort_test.cpp
+++ src/main/cpp/operators/secure_sort_test.cpp
@@ -1,27 +1,53 @@
 //
 // Created by Salome Kariuki on 4/29/20.
 //
-#include <gtest/gtest.h>
 #include "secure_sort.h"
-#include <data/PQDataProvider.h>
 #include <ctime>
+#include <data/PQDataProvider.h>
+#include <gflags/gflags.h>
 
-class secure_join_test : public ::testing::Test {
-protected:
-    void SetUp() override {};
-    void TearDown() override{};
-};
+#include "emp-sh2pc/emp-sh2pc.h"
+#include "emp-tool/emp-tool.h"
+#include "querytable/private_share_utility.h"
+#include <chrono>
 
-TEST_F(secure_sort_test, simple_execution_test) {
-PQDataProvider pq;
-auto qt = pq.GetQueryTable("dbname=tpch_sf1", "SELECT * FROM customer LIMIT 10");
-vector<int> ordinals{1,2,3};
-SortDef def;
-def.order = SortOrder::ASCENDING;
-def.ordinals = ordinals;
-clock_t time_req;
-time_req = clock();
-Sort(qt.get(), def);
-time_req = clock() - time_req;
-std::cout << "Running sort operation took: " << time_req << std::endl;
+DEFINE_int32(party, 1, "party for EMP execution");
+DEFINE_int32(port, 43439, "port for EMP execution");
+DEFINE_string(hostname, "127.0.0.1", "hostname for execution");
+DEFINE_bool(input, false, "input value");
+DEFINE_int32(int_input, 10, "int_intput");
+
+int main(int argc, char **argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true /* remove_flags */);
+  emp::NetIO *io = new emp::NetIO(
+      FLAGS_party == emp::ALICE ? nullptr : FLAGS_hostname.c_str(), FLAGS_port);
+  setup_semi_honest(io, FLAGS_party);
+
+  PQDataProvider pq;
+  auto lineitem =
+      pq.GetQueryTable("dbname=tpch_alice", "SELECT c_custkey FROM customer LIMIT 10");
+
+  EmpParty my_party =
+      FLAGS_party == emp::ALICE ? EmpParty::ALICE : EmpParty::BOB;
+
+  ShareDef def;
+  ShareCount ca = {.party = EmpParty::ALICE};
+  ca.num_tuples = lineitem->GetNumTuples();
+  ShareCount cb = {.party = EmpParty::BOB};
+  cb.num_tuples = lineitem->GetNumTuples();
+
+  def.share_map[EmpParty::ALICE] = ca;
+  def.share_map[EmpParty::BOB] = cb;
+  // lineitem.get()->GetTuple(0)->GetField(ord)
+  auto s_lineitem =
+      ShareData(lineitem->GetSchema(), my_party, lineitem.get(), def);
+
+  vector<int> ordinals{0};
+  SortDef sortdef;
+  sortdef.order = SortOrder::ASCENDING;
+  sortdef.ordinals = ordinals;
+  // cout << ((HalfGateGen<NetIO> *)CircuitExecution::circ_exec)->gid<<endl;
+  Sort(s_lineitem.get(), sortdef);
+  // std::cout << "Running sort operation took: " << std::endl;
+  // cout << ((HalfGateGen<NetIO> *)CircuitExecution::circ_exec)->gid<<endl;
 }
