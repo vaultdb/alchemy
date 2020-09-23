28f817e264e890b581598483438df001715d2d68
Jennie Rogers <jennie@northwestern.edu>
2020-09-22 20:04:35 -0500

    added and passed test for three-way join

--- src/main/cpp/test/basic_join_test.cpp
+++ src/main/cpp/test/basic_join_test.cpp
@@ -52,7 +52,6 @@ TEST_F(BasicJoinTest, test_tpch_q3_customer_orders) {
                                         "FROM customer_cte, orders_cte "
                                         "ORDER BY o_orderkey, o_custkey, o_orderdate, o_shippriority, c_custkey";
 
-   std::cout << "Expected results SQL: " << expectedResultSql <<  std::endl;
 
    std::shared_ptr<QueryTable> expected = DataUtilities::getQueryResults(dbName, expectedResultSql, true);
 
@@ -120,13 +119,75 @@ TEST_F(BasicJoinTest, test_tpch_q3_lineitem_orders) {
 
     std::shared_ptr<QueryTable> observed = joinOp->run();
 
-   
+
 
     ASSERT_EQ(observed->toString(false), expected->toString(false));
     ASSERT_EQ(*expected, *observed);
 
 }
+
+
+
 // compose C-O-L join should produce one output tuple, order ID 210945
+TEST_F(BasicJoinTest, test_tpch_q3_lineitem_orders_customer) {
+
+    // get inputs from local oblivious ops
+    // first 3 customers, propagate this constraint up the join tree for the test
+    std::string customerSql = "SELECT c_custkey, c_mktsegment <> 'HOUSEHOLD' cdummy "
+                              "FROM customer  "
+                              "WHERE c_custkey <= 5 "
+                              "ORDER BY c_custkey";
+
+
+
+    std::string ordersSql = "SELECT o_orderkey, o_custkey, o_orderdate, o_shippriority, o_orderdate >= date '1995-03-25' odummy "
+                            "FROM orders "
+                            "WHERE o_custkey <= 5 "
+                            "ORDER BY o_orderkey, o_custkey, o_orderdate, o_shippriority ";
+
+    std::string lineitemSql = "SELECT  l_orderkey, l_extendedprice * (1 - l_discount) revenue, l_shipdate <= date '1995-03-25' ldummy "
+                              "FROM lineitem "
+                              "WHERE l_orderkey IN (SELECT o_orderkey FROM orders where o_custkey <= 5)  "
+                              "ORDER BY l_orderkey, revenue ";
+
+    std::string expectedResultSql = "WITH orders_cte AS (" + ordersSql + "), "
+                                          "lineitem_cte AS (" + lineitemSql + "), "
+                                           "customer_cte AS (" + customerSql + ") "
+                                                 "SELECT l_orderkey, revenue, o_orderkey, o_custkey, o_orderdate, o_shippriority, c_custkey, (cdummy OR odummy OR ldummy OR o_orderkey <> l_orderkey OR c_custkey <> o_custkey) dummy "
+                                                 "FROM lineitem_cte, orders_cte, customer_cte "
+                                                 "ORDER BY l_orderkey, revenue, o_orderkey, o_custkey, o_orderdate, o_shippriority, c_custkey";
+
+    std::shared_ptr<QueryTable> expected = DataUtilities::getQueryResults(dbName, expectedResultSql, true);
+
+
+    std::shared_ptr<Operator> customerInput(new SqlInput(dbName, customerSql, true));
+    std::shared_ptr<Operator> ordersInput(new SqlInput(dbName, ordersSql, true));
+    std::shared_ptr<Operator> lineitemInput(new SqlInput(dbName, lineitemSql, true));
+
+
+    ConjunctiveEqualityPredicate customerOrdersOrdinals;
+    customerOrdersOrdinals.push_back(EqualityPredicate (1, 0)); //  o_custkey, c_custkey
+    std::shared_ptr<BinaryPredicate> customerOrdersPredicate(new JoinEqualityPredicate(customerOrdersOrdinals, false));
+
+    ConjunctiveEqualityPredicate lineitemOrdersOrdinals;
+    lineitemOrdersOrdinals.push_back(EqualityPredicate (0, 0)); //  l_orderkey, o_orderkey
+    std::shared_ptr<BinaryPredicate> lineitemOrdersPredicate(new JoinEqualityPredicate(lineitemOrdersOrdinals, false));
+
+
+    BasicJoin *customerOrdersJoin = new BasicJoin(customerOrdersPredicate, ordersInput, customerInput);
+
+    BasicJoin *fullJoin = new BasicJoin(lineitemOrdersPredicate, lineitemInput, customerOrdersJoin->getPtr());
+
+
+    std::shared_ptr<QueryTable> observed = fullJoin->run();
+
+
+
+    ASSERT_EQ(observed->toString(false), expected->toString(false));
+    ASSERT_EQ(*expected, *observed);
+
+}
+
 
 
 
