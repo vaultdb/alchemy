ef03d28aad497af2cd182b22c25ca43698e18c81
Shashank Satish <shashank-satish-d@u.northwestern.edu>
2020-10-12 19:23:18 -0500

    Linker error fix for float added

--- deps/emp/install.sh
+++ deps/emp/install.sh
@@ -1,2 +1,7 @@
 git clone https://github.com/emp-toolkit/emp-readme.git
 python ./emp-readme/scripts/install.py -install -tool -ot -sh2pc
+
+cd emp-tool
+cmake -DENABLE_FLOAT=On .
+make clean all
+sudo make install
