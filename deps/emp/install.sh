dd490f97df1a7cac404f6e2d9b43b7616bd76695
Shashank Satish <shashank-satish-d@u.northwestern.edu>
2020-10-12 19:12:19 -0500

    emp: verfied python installation

--- deps/emp/install.sh
+++ deps/emp/install.sh
@@ -1,2 +1,2 @@
 git clone https://github.com/emp-toolkit/emp-readme.git
-python3 ./emp-readme/scripts/install.py -install -tool -ot -sh2pc
+python ./emp-readme/scripts/install.py -install -tool -ot -sh2pc
