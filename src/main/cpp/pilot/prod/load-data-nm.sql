a608d482e54ae0f3067814b50e41878586c1a51a
Jennie Rogers <jennie.rogers@northwestern.edu>
2021-12-16 18:57:15 +0000

    small script to fix null representation in pilot data

--- src/main/cpp/pilot/prod/load-data-nm.sql
+++ src/main/cpp/pilot/prod/load-data-nm.sql
@@ -1,6 +1,9 @@
 -- stitch it all together into a single DB for validation
+-- start with pilot/prod/clean-nulls.sh first
 \c enrich_htn_prod
 \set ECHO all
+
+
 
 \i 'pilot/prod/load-population-labels-nm.sql'
 \i 'pilot/prod/load-demographics-nm.sql'
