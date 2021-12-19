ffda57d31859a9e4a72c216c8f259020a9a734a1
Jennie Rogers <jennie@northwestern.edu>
2021-12-19 01:40:02 +0000

    added sketch for testing pilot code on subset of real data

---
+++ src/main/cpp/test-pilot.sh
@@ -0,0 +1,16 @@
+
+bash pilot/prod/clean-nulls.sh 
+
+dropdb enrich_htn_prod
+
+
+createdb enrich_htn_prod
+psql enrich_htn_prod -v ON_ERROR_STOP=on < pilot/prod/load-data-nm.sql
+
+./bin/secret_share_csv pilot/input/chi-prod-patient.csv pilot/secret_shares/chi-prod-patient
+
+#warmup
+./bin/load_tuples_data_partner 127.0.0.1 54321 1 pilot/input/alice-prod-patient.csv  pilot/secret_shares/chi-prod-patient.alice &                                                                                                                          
+./bin/load_tuples_data_partner 127.0.0.1 54321 2 pilot/input/bob-prod-patient.csv   pilot/secret_shares/chi-prod-patient.bob                                                                                                                                         
+
+
