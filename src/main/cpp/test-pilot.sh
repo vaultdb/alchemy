2acc9bd0e354616990ffec6b1c4a55622c66c6f4
Jennie Rogers <jennie@northwestern.edu>
2021-12-22 00:22:32 +0000

    fine-tuning scripts for pilot

--- src/main/cpp/test-pilot.sh
+++ src/main/cpp/test-pilot.sh
@@ -14,7 +14,10 @@ psql enrich_htn_prod -v ON_ERROR_STOP=on < pilot/prod/load-data-nm.sql
 ./bin/secret_share_csv pilot/input/chi-prod-patient.csv pilot/secret_shares/chi-prod-patient
 
 #warmup
-./bin/load_tuples_data_partner 127.0.0.1 54321 1 pilot/input/alice-prod-patient.csv  pilot/secret_shares/chi-prod-patient.alice &                                                                                                                          
-./bin/load_tuples_data_partner 127.0.0.1 54321 2 pilot/input/bob-prod-patient.csv   pilot/secret_shares/chi-prod-patient.bob                                                                                                                                         
+#./bin/load_tuples_data_partner 127.0.0.1 54321 1 pilot/input/alice-prod-patient.csv  pilot/secret_shares/chi-prod-patient.alice &                                          
+#./bin/load_tuples_data_partner 127.0.0.1 54321 2 pilot/input/bob-prod-patient.csv   pilot/secret_shares/chi-prod-patient.bob
+
+./bin/run_data_partner 127.0.0.1 54321 1 pilot/input/alice-prod-patient.csv  pilot/secret_shares/chi-prod-patient.alice &
+./bin/run_data_partner 127.0.0.1 54321 2 pilot/input/bob-prod-patient.csv  pilot/secret_shares/chi-prod-patient.bob
 
 
