1f80cdf5ea29fc4bdba724465fa1591bb1e77567
Jennie Rogers <jennie@northwestern.edu>
2022-01-19 01:13:04 -0600

    setting up no-dedupe pipeline for all study years

--- src/main/cpp/pilot/src/run_data_partner_aggregation.cpp
+++ src/main/cpp/pilot/src/run_data_partner_aggregation.cpp
@@ -19,13 +19,17 @@ using namespace emp;
 auto start_time = emp::clock_start();
 auto cumulative_runtime = emp::time_from(start_time);
 
-string partial_aggregate_query = "WITH aggs AS (SELECT age_strata, sex, ethnicity, race, SUM(CASE WHEN numerator AND NOT denom_excl THEN 1 ELSE 0 END)::INT numerator_cnt, "
-                                                              "SUM(CASE WHEN NOT denom_excl THEN 1 ELSE 0 END)::INT denominator_cnt \n"
-                                 " FROM patient \n"
-                                 " GROUP BY age_strata, sex, ethnicity, race) \n"
-                                 "SELECT d.*, COALESCE(numerator_cnt, 0) numerator_cnt, COALESCE(denominator_cnt, 0) denominator_cnt \n"
-                                 "FROM demographics_domain d LEFT JOIN aggs a on d.age_strata = a.age_strata  AND d.sex = a.sex  AND d.ethnicity = a.ethnicity AND d.race = a.race "
-                                 "ORDER BY age_strata, sex, ethnicity, race ";
+string partial_aggregate_query = "WITH cohort AS (SELECT DISTINCT  pat_id, min(age_strata) age_strata, sex, ethnicity, race, numerator, denom_excl\n"
+                                 "                FROM patient\n"
+                                 "                GROUP BY pat_id, sex, ethnicity, race, numerator, denom_excl\n"
+                                 "                ORDER BY pat_id),\n"
+                                 "     aggs AS (SELECT age_strata, sex, ethnicity, race, SUM(CASE WHEN numerator AND NOT denom_excl THEN 1 ELSE 0 END)::INT numerator_cnt,\n"
+                                 "                                                       SUM(CASE WHEN NOT denom_excl THEN 1 ELSE 0 END)::INT denominator_cnt\n"
+                                 "                                  FROM cohort\n"
+                                 "                                  GROUP BY age_strata, sex, ethnicity, race)\n"
+                                 "SELECT d.*, COALESCE(numerator_cnt, 0) numerator_cnt, COALESCE(denominator_cnt, 0) denominator_cnt\n"
+                                 "FROM demographics_domain d LEFT JOIN aggs a on d.age_strata = a.age_strata  AND d.sex = a.sex  AND d.ethnicity = a.ethnicity AND d.race = a.race\n"
+                                 "ORDER BY age_strata, sex, ethnicity, race";
 
 
 
