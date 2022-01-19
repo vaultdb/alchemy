f96b9f4d9627027524961139f3a164d6c9580766
Jennie Rogers <jennie@northwestern.edu>
2022-01-19 13:27:58 -0600

    refining runner script for experiments

--- src/main/cpp/pilot/prod/run-data-partner-nm.sh
+++ src/main/cpp/pilot/prod/run-data-partner-nm.sh
@@ -7,4 +7,5 @@ fi
 YEAR=$1
 ./bin/run_data_partner -h 127.0.0.1 -P 4444 --party=1 -d enrich_htn_prod -y $YEAR \
 		       --rp=pilot/secret_shares/alliance/alliance-partial-counts-$YEAR.alice \
-		       --rc=pilot/secret_shares/alliance/alliance-partial-counts-$YEAR.alice 
+		       --rc=pilot/secret_shares/alliance/alliance-multisite-$YEAR.alice \
+		       -l pilot/logs/vaultdb-data-partner-2018-alice
--- src/main/cpp/pilot/prod/run-data-partner-rush.sh
+++ src/main/cpp/pilot/prod/run-data-partner-rush.sh
@@ -6,6 +6,10 @@ fi
 
 YEAR=$1
 
+#may need to run ssh setup first with:
+# ssh -p 3333 -f -N -i ~/.ssh/vaultdb-nm-key -L 4444:127.0.0.1:4444 vaultdb@165.124.123.122
+
 ./bin/run_data_partner -h 127.0.0.1 -P 4444 --party=2 -d enrich_htn_prod -y $YEAR \
 		       --rp=pilot/secret_shares/alliance/alliance-partial-counts-$YEAR.bob \
-		       --rc=pilot/secret_shares/alliance/alliance-partial-counts-$YEAR.bob 
+		       --rc=pilot/secret_shares/alliance/alliance-multisite-$YEAR.bob \
+		       -l pilot/logs/vaultdb-data-partner-2018-bob
