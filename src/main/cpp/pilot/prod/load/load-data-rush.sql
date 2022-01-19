a54f42f1d0d05426aeca2e065ae646b50c9d4f98
Jennie Rogers <jennie@northwestern.edu>
2022-01-18 22:34:09 -0600

    more setup for semijoin optimization

--- src/main/cpp/pilot/prod/load/load-data-rush.sql
+++ src/main/cpp/pilot/prod/load/load-data-rush.sql
@@ -8,6 +8,17 @@
 \i 'pilot/prod/load/rush/load-population-labels.sql'
 \i 'pilot/prod/load/rush/assemble-patient-table.sql'
 
+-- set up multisite flags
+DROP TABLE IF EXISTS multisite;
+CREATE TEMP TABLE multisite (pat_id INT, study_year int);
+\copy multisite FROM 'pilot/input/rush-multisite.csv' CSV HEADER
+ALTER TABLE patient ADD COLUMN multisite bool DEFAULT false;
+
+UPDATE patient p SET multisite=true
+FROM multisite m
+WHERE m.pat_id = p.pat_id AND m.study_year = p.study_year;
+
+
 
 
 \copy (SELECT pat_id, age_strata, sex, ethnicity, race, numerator, denom_excl FROM patient WHERE study_year=2018 ORDER BY pat_id, site_id) TO 'pilot/input/alice-prod-patient.csv' CSV
@@ -34,16 +45,6 @@
 
 
 \set ECHO none
--- set up multisite flags
-DROP TABLE IF EXISTS multisite;
-CREATE TEMP TABLE multisite (pat_id INT, study_year int);
-\copy multisite FROM 'pilot/input/rush-multisite.csv' CSV HEADER
-ALTER TABLE patient ADD COLUMN multisite bool DEFAULT false;
-
-UPDATE patient p SET multisite=true
-FROM multisite m
-WHERE m.pat_id = p.pat_id AND m.study_year = p.study_year;
-
 \i 'pilot/prod/load/verify-domain.sql'
 \i 'pilot/prod/load/generate-demographics-domain.sql'
 
