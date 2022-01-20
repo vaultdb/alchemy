2ab132184593d47ccd684a20dffa81d19b9f6f8d
Jennie Rogers <jennie@northwestern.edu>
2022-01-20 07:03:48 +0000

    rounding out scripts for pilot

---
+++ src/main/cpp/pilot/prod/run-data-partner-aggregation-nm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "usage: ./pilot/prod/run-data-partner-aggregation-nm.sh <test \in (2018-2020, all)>"
+    exit
+fi
+
+YEAR=$1
+# May need to connect to NM first with:
+# ssh -p 3333 -f -N -i ~/.ssh/vaultdb-nm-key -L 4444:127.0.0.1:4444 vaultdb@165.124.123.122
+./bin/run_data_partner_aggregation 127.0.0.1 4444 2 enrich_htn_prod $YEAR pilot/secret_shares/alliance/alliance-partial-counts-no-dedupe-$YEAR.bob
+
+
