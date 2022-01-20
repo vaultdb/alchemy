35e01f11706347ea491839915772021b86bffa98
Jennie Rogers <jennie@northwestern.edu>
2022-01-20 06:53:00 +0000

    fixed a typo in pilot script

--- src/main/cpp/pilot/prod/run-data-partner-suite-rush.sh
+++ src/main/cpp/pilot/prod/run-data-partner-suite-rush.sh
@@ -2,7 +2,7 @@
 
 for TEST in 2018 2019 2020 all
 do
-    ./pilot/prod/run-data-partner-rush.sh $TEST
-    ./pilot/prod/run-data-partner-aggregation-rush.sh $TEST
+    bash ./pilot/prod/run-data-partner-rush.sh $TEST
+    bash ./pilot/prod/run-data-partner-aggregation-rush.sh $TEST
 done
 
