fb2246124dbbd6ef1f395b02a582192a43f58748
Jennie Rogers <jennie@northwestern.edu>
2022-01-20 23:15:56 +0000

    refining pilot scripts

--- src/main/cpp/pilot/prod/run-data-partner-rush.sh
+++ src/main/cpp/pilot/prod/run-data-partner-rush.sh
@@ -12,4 +12,4 @@ YEAR=$1
 ./bin/run_data_partner -h 127.0.0.1 -P 4444 --party=2 -d enrich_htn_prod -y $YEAR \
 		       --rp=pilot/secret_shares/alliance/alliance-partial-counts-$YEAR.bob \
 		       --rc=pilot/secret_shares/alliance/alliance-multisite-$YEAR.bob \
-		       -l pilot/logs/vaultdb-data-partner-2018-bob
+		       -l pilot/logs/vaultdb-data-partner-$YEAR
