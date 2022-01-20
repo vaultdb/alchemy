c17fe41b29c0dccfde774ca2e5c48a2f40b5e752
Jennie Rogers <jennie@northwestern.edu>
2022-01-20 06:50:22 +0000

    fixed a typo in pilot script

--- src/main/cpp/pilot/prod/run-data-partner-suite-nm.sh
+++ src/main/cpp/pilot/prod/run-data-partner-suite-nm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for $TEST in 2018 2019 2020 all
+for TEST in 2018 2019 2020 all
 do
     ./pilot/prod/run-data-partner-nm.sh $TEST
     ./pilot/prod/run-data-partner-aggregation-nm.sh $TEST
