f45ed80fa09568622ea94b9bdc2b139a05b1a6b9
Jennie Rogers <jennie@northwestern.edu>
2022-01-24 18:05:35 -0600

    cleaning up loader scripts

--- src/main/cpp/pilot/queries/patient-2019.sql
+++ src/main/cpp/pilot/queries/patient-2019.sql
@@ -1,3 +1,4 @@
 SELECT  pat_id, age_strata, sex, ethnicity, race, numerator, denom_excl
 FROM patient
+WHERE study_year=2019
 ORDER BY pat_id, age_strata, sex, ethnicity, race, numerator, denom_excl
--- src/main/cpp/pilot/queries/patient-2020.sql
+++ src/main/cpp/pilot/queries/patient-2020.sql
@@ -1,3 +1,4 @@
 SELECT  pat_id, age_strata, sex, ethnicity, race, numerator, denom_excl
 FROM patient
+WHERE study_year=2020
 ORDER BY pat_id, age_strata, sex, ethnicity, race, numerator, denom_excl
