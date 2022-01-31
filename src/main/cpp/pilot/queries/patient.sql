5b456f657e5df1f56fa0e3275aa12330657975da
Jennie Rogers <jennie@northwestern.edu>
2022-01-31 16:57:37 -0600

    refining queries for pilot deployment

--- src/main/cpp/pilot/queries/patient.sql
+++ src/main/cpp/pilot/queries/patient.sql
@@ -1,4 +1,4 @@
-SELECT  pat_id, age_strata, sex, ethnicity, race, numerator, denom_excl
+SELECT DISTINCT pat_id, age_strata, sex, ethnicity, race, numerator, denom_excl
 FROM patient
 WHERE :selection
 ORDER BY pat_id, age_strata, sex, ethnicity, race, numerator, denom_excl
