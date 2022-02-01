9403323873ecd4aa72bc7f09e5b8dc6953089405
Jennie Rogers <jennie@northwestern.edu>
2022-01-31 18:20:57 -0600

    aligning batch scripts with secret share locations for prod

--- src/main/cpp/pilot/prod/run-data-partner-batch-nm.sh
+++ src/main/cpp/pilot/prod/run-data-partner-batch-nm.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "usage: ./pilot/prod/run-data-partner-nm.sh <test \in (2018-2020, all)>"
+# only "all" available in secret shares right now
+
+if [ "$#" -ne 2]; then
+    echo "usage: ./pilot/prod/run-data-partner-nm.sh <test \in (2018-2020, all)> <batch count>"
     exit
 fi
 
 YEAR=$1
+BATCHES=$2
+
 ./bin/run_data_partner_batch -h 127.0.0.1 -P 4444 --party=1 -d enrich_htn_prod -y $YEAR \
-		       -r pilot/secret_shares/alliance/multisite-$YEAR.alice \
+		       -r pilot/secret_shares/batch/$BATCHES/alliance-all \
 		       -l pilot/logs/vaultdb-data-partner-batch-$YEAR \
-		       -s --batch-count=10
+		       -s --batch-count=$BATCHES
--- src/main/cpp/pilot/prod/run-data-partner-batch-rush.sh
+++ src/main/cpp/pilot/prod/run-data-partner-batch-rush.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then
-    echo "usage: ./pilot/prod/run-data-partner-rush.sh <test \in (2018-2020, all)>"
+
+# only "all" available in secret shares right now
+if [ "$#" -ne 2 ]; then
+    echo "usage: ./pilot/prod/run-data-partner-rush.sh <test \in (2018-2020, all)> <batch count>"
     exit
 fi
 
 YEAR=$1
+BATCHES=$2
 
 #may need to run ssh setup first with:
 # ssh -p 3333 -f -N -i ~/.ssh/vaultdb-nm-key -L 4444:127.0.0.1:4444 vaultdb@165.124.123.122
 
 ./bin/run_data_partner_batch -h 127.0.0.1 -P 4444 --party=2 -d enrich_htn_prod -y $YEAR \
-		       -r pilot/secret_shares/alliance/multisite-$YEAR.bob \
+		       -r pilot/secret_shares/batch/$BATCHES/alliance-all \
 		       -l pilot/logs/vaultdb-data-partner-batch-$YEAR \
-		       --batch-count=10
+		       --batch-count=$BATCHES
