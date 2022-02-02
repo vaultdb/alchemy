5112c0b9863054becff6d5b1449062020f816a41
Jennie Rogers <jennie@northwestern.edu>
2022-02-02 14:07:18 -0600

    fine-tuning pilot scripts

--- src/main/cpp/pilot/prod/run-batches-nm.sh
+++ src/main/cpp/pilot/prod/run-batches-nm.sh
@@ -1,17 +1,19 @@
 #!/bin/bash -x
 
 
+LOG=pilot/logs/perf-measurements.log
 
+echo "Starting new run" >> $LOG
 for YEAR in 2018 2019 2020 all
 do
     echo 'Evaluating study_year='$YEAR
-    ./pilot/prod/run-data-partner-aggregation-nm.sh $YEAR
-    ./pilot/prod/run-data-partner-nm.sh $YEAR
+    ./pilot/prod/run-data-partner-aggregation-nm.sh $YEAR 
+    ./pilot/prod/run-data-partner-nm.sh $YEAR  >> $LOG
     for B in 10 20 30 40 50
     do
 	
 	echo 'Running for '$YEAR' batch count: '$B
-	./pilot/prod/run-data-partner-batch-nm.sh $YEAR $B
+	./pilot/prod/run-data-partner-batch-nm.sh $YEAR $B  >> $LOG
     done
 done
 
