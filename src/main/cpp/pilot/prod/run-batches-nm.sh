5117761ba4e925a3fbe0f84236a09be7330ccc6f
Jennie Rogers <jennie@northwestern.edu>
2022-02-02 13:49:44 -0600

    polishing up scripts for pilot runs

--- src/main/cpp/pilot/prod/run-batches-nm.sh
+++ src/main/cpp/pilot/prod/run-batches-nm.sh
@@ -4,7 +4,9 @@
 
 for YEAR in 2018 2019 2020 all
 do
-
+    echo 'Evaluating study_year='$YEAR
+    ./pilot/prod/run-data-partner-aggregation-nm.sh $YEAR
+    ./pilot/prod/run-data-partner-nm.sh $YEAR
     for B in 10 20 30 40 50
     do
 	
--- src/main/cpp/pilot/prod/run-batches-rush.sh
+++ src/main/cpp/pilot/prod/run-batches-rush.sh
@@ -4,7 +4,9 @@
 
 for YEAR in 2018 2019 2020 all
 do
-
+    echo 'Evaluating study_year='$YEAR
+    ./pilot/prod/run-data-partner-aggregation-rush.sh $YEAR
+    ./pilot/prod/run-data-partner-rush.sh $YEAR
     for B in 10 20 30 40 50
     do
 	
