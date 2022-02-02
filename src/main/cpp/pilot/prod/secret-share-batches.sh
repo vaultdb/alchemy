c8f17e44cb76fc1ad7829050a5332f6a7b837e8a
Jennie Rogers <jennie@northwestern.edu>
2022-02-02 18:28:28 +0000

    adding script to generate secret shares

---
+++ src/main/cpp/pilot/prod/secret-share-batches.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+
+for B in 10 20 30 40 50
+do
+    mkdir -p pilot/secret_shares/batch/$B
+    for YEAR in 2018 2019 2020 all
+    do
+	echo 'Secret sharing for batch '$B' in study year '$YEAR
+	./bin/secret_share_batch_from_query -D enrich_htn_prod -q pilot/queries/patient.sql  -y $YEAR -b $B -d pilot/secret_shares/batch/$B/alliance-$YEAR
+    done
+done
+
