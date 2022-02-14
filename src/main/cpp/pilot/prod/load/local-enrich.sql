712758045952f3ce46038a787da5c2263922bf76
Jennie Rogers <jennie@northwestern.edu>
2022-02-14 18:30:49 +0000

    adding diagnostic script

---
+++ src/main/cpp/pilot/prod/load/local-enrich.sql
@@ -0,0 +1,6 @@
+\set col age_strata
+
+SELECT :col, SUM(CASE WHEN numerator THEN 1 else 0 END) numerator_cnt, SUM(CASE WHEN denom THEN 1 ELSE 0 END) denom_cnt
+FROM patient
+GROUP BY :col
+ORDER BY :col;
