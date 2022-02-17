fcd9ac8cf241a933bb172de08f4312b42425d551
Jennie Rogers <jennie@northwestern.edu>
2022-02-17 22:06:31 +0000

    refining diagnostics script

--- src/main/cpp/pilot/queries/local-enrich.sql
+++ src/main/cpp/pilot/queries/local-enrich.sql
@@ -1,12 +1,12 @@
 -- example execution:
--- psql enrich_htn_prod --csv -v col=age_strata -v selection='study_year=2018' < pilot/queries/local-enrich.sql
+-- psql enrich_htn_prod --csv -v col=age_strata  < pilot/queries/local-enrich.sql
 
-SELECT :col, sum(numerator::INT) numerator_cnt, SUM(denominator::INT) denominator_cnt,
+SELECT study_year, :col, sum(numerator::INT) numerator_cnt, SUM(denominator::INT) denominator_cnt,
        SUM(CASE WHEN numerator AND multisite THEN 1 ELSE 0 END) numerator_multisite_cnt,
        SUM(CASE WHEN denominator AND multisite THEN 1 ELSE 0 END) denominator_multisite_cnt
 FROM patient
-WHERE :selection AND NOT denom_excl
-GROUP BY :col
-ORDER BY :col;
+WHERE NOT denom_excl
+GROUP BY  study_year, :col
+ORDER BY  study_year, :col;
 
 
