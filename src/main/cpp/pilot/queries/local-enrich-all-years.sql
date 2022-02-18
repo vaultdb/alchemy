40076b3904d1abe70e9c3ba6e374cc1204fb9509
Jennie Rogers <jennie@northwestern.edu>
2022-02-18 22:11:04 +0000

    more diagnostic scripting

--- src/main/cpp/pilot/queries/local-enrich-all-years.sql
+++ src/main/cpp/pilot/queries/local-enrich-all-years.sql
@@ -11,7 +11,7 @@ CREATE TEMP TABLE distinct_patient AS (
        SELECT DISTINCT site_id, pat_id, age_strata, sex, ethnicity, race, numerator, denominator, denom_excl, multisite
        FROM patient);
        
-SELECT  site_id, :col_name stratification, :col, sum(numerator::INT) numerator_cnt, SUM(denominator::INT) denominator_cnt,
+SELECT  0 study_year, site_id, :col_name stratification, :col, sum(numerator::INT) numerator_cnt, SUM(denominator::INT) denominator_cnt,
        SUM(CASE WHEN numerator AND multisite THEN 1 ELSE 0 END) numerator_multisite_cnt,
        SUM(CASE WHEN denominator AND multisite THEN 1 ELSE 0 END) denominator_multisite_cnt
 FROM distinct_patient
@@ -24,7 +24,7 @@ ORDER BY  site_id, :col;
 \set col sex
 \set col_name :quote:col:quote
 
-SELECT site_id, :col_name stratification, :col, sum(numerator::INT) numerator_cnt, SUM(denominator::INT) denominator_cnt,
+SELECT  0 study_year, site_id, :col_name stratification, :col, sum(numerator::INT) numerator_cnt, SUM(denominator::INT) denominator_cnt,
        SUM(CASE WHEN numerator AND multisite THEN 1 ELSE 0 END) numerator_multisite_cnt,
        SUM(CASE WHEN denominator AND multisite THEN 1 ELSE 0 END) denominator_multisite_cnt
 FROM  distinct_patient
@@ -37,7 +37,7 @@ ORDER BY  site_id, :col;
 \set col ethnicity
 \set col_name :quote:col:quote
 
-SELECT site_id, :col_name stratification, :col, sum(numerator::INT) numerator_cnt, SUM(denominator::INT) denominator_cnt,
+SELECT  0 study_year, site_id, :col_name stratification, :col, sum(numerator::INT) numerator_cnt, SUM(denominator::INT) denominator_cnt,
        SUM(CASE WHEN numerator AND multisite THEN 1 ELSE 0 END) numerator_multisite_cnt,
        SUM(CASE WHEN denominator AND multisite THEN 1 ELSE 0 END) denominator_multisite_cnt
 FROM  distinct_patient
@@ -50,7 +50,7 @@ ORDER BY  site_id, :col;
 \set col race
 \set col_name :quote:col:quote
 
-SELECT site_id, :col_name stratification, :col, sum(numerator::INT) numerator_cnt, SUM(denominator::INT) denominator_cnt,
+SELECT  0 study_year, site_id, :col_name stratification, :col, sum(numerator::INT) numerator_cnt, SUM(denominator::INT) denominator_cnt,
        SUM(CASE WHEN numerator AND multisite THEN 1 ELSE 0 END) numerator_multisite_cnt,
        SUM(CASE WHEN denominator AND multisite THEN 1 ELSE 0 END) denominator_multisite_cnt
 FROM  distinct_patient
