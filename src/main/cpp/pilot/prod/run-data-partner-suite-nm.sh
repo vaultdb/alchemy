c67b9064fabfe11829f79444ff9b8551a63ec1b2
Jennie Rogers <jennie@northwestern.edu>
2022-02-19 23:05:06 -0600

    updating test runners

--- src/main/cpp/pilot/prod/run-data-partner-suite-nm.sh
+++ src/main/cpp/pilot/prod/run-data-partner-suite-nm.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-for TEST in 2018 2019 2020 all
+for TEST in 2018 2018-2019 all
 do
-    bash pilot/prod/run-data-partner-nm.sh $TEST
     bash pilot/prod/run-data-partner-aggregation-nm.sh $TEST
+    bash pilot/prod/run-data-partner-nm.sh $TEST
+    bash pilot/prod/run-data-partner-no-semi-join-nm.sh  $TEST
 done
 
+for BATCH in 25 50 75 100
+do
+    
+    for TEST in 2018 2018-2019 all
+    do
+	bash pilot/prod/run-data-partner-batch-nm.sh $TEST $BATCH
+    done
+done
--- src/main/cpp/pilot/prod/run-data-partner-suite-rush.sh
+++ src/main/cpp/pilot/prod/run-data-partner-suite-rush.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-for TEST in 2018 2019 2020 all
+for TEST in 2018 2018-2019 all
 do
-    bash pilot/prod/run-data-partner-rush.sh $TEST
     bash pilot/prod/run-data-partner-aggregation-rush.sh $TEST
+    bash pilot/prod/run-data-partner-rush.sh $TEST
+    bash pilot/prod/run-data-partner-no-semi-join-rush.sh  $TEST
 done
 
+for BATCH in 25 50 75 100
+do
+    
+    for TEST in 2018 2018-2019 all
+    do
+	bash pilot/prod/run-data-partner-batch-rush.sh $TEST $BATCH
+    done
+done
