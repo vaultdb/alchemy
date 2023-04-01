b2d62c6006390d55c80db8488eccbf97435e40d8
Jennie Rogers <5811507+jennierogers@users.noreply.github.com>
2023-04-01 15:24:42 -0500

    tpch truncating algo mod'd to keep pk/fkey relns

--- src/main/cpp/truncate-tpch-tables.sql
+++ src/main/cpp/truncate-tpch-tables.sql
@@ -2,44 +2,25 @@ DROP FUNCTION IF EXISTS truncate_tables;
 CREATE FUNCTION truncate_tables(cutoff integer) RETURNS VOID AS
 $BODY$
 BEGIN
--- prevent it from deleting parts and suppliers that appear in more than one order
-EXECUTE 'ALTER TABLE lineitem DROP CONSTRAINT lineitem_l_partkey_l_suppkey_fkey';
-
--- cascades to orders, lineitem
 RAISE NOTICE 'Deleting rows from customer, orders, and lineitem';
 EXECUTE 'DELETE FROM customer WHERE c_custkey > ' || cutoff;
 
 RAISE NOTICE 'Deleting rows from partsupp';
 
-EXECUTE 'WITH to_delete AS (
-    (SELECT ps_partkey, ps_suppkey FROM partsupp)
-        EXCEPT
-        (SELECT DISTINCT l_partkey, l_suppkey FROM lineitem))
-DELETE FROM partsupp
-    WHERE (ps_partkey, ps_suppkey) IN (SELECT * FROM to_delete)';
+EXECUTE 'DELETE FROM partsupp
+    WHERE NOT EXISTS (SELECT  * FROM lineitem l  WHERE l_partkey = ps_partkey AND l_suppkey = ps_suppkey);';
 
 RAISE NOTICE 'Deleting rows from supplier';
 
-EXECUTE 'WITH to_delete AS (
-    (SELECT s_suppkey FROM supplier)
-        EXCEPT
-        (SELECT DISTINCT ps_suppkey FROM partsupp))
-DELETE FROM supplier
-    WHERE s_suppkey IN (SELECT * FROM to_delete)';
+EXECUTE 'DELETE FROM supplier
+WHERE NOT EXISTS (SELECT * FROM partsupp WHERE ps_suppkey = s_suppkey)';
 
 
 RAISE NOTICE 'Deleting rows from part';
 
-EXECUTE 'WITH to_delete AS (
-    (SELECT p_partkey FROM part)
-        EXCEPT
-        (SELECT DISTINCT ps_partkey FROM partsupp))
-DELETE FROM part
-    WHERE p_partkey IN (SELECT * FROM to_delete)';
+EXECUTE 'DELETE FROM part
+WHERE NOT EXISTS (SELECT * FROM partsupp WHERE ps_partkey = p_partkey);';
 
-
-EXECUTE 'ALTER TABLE lineitem ADD CONSTRAINT lineitem_l_partkey_l_suppkey_fkey FOREIGN KEY (l_partkey , l_suppkey) ' ||
-'REFERENCES partsupp(ps_partkey, ps_suppkey)';
 
 END
 $BODY$
