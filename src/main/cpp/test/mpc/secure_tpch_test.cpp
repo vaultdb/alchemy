c06d16ee3809dd65db138ac4c0e5eb6ef8b685f7
Jennie Rogers <jennie@northwestern.edu>
2023-04-07 18:04:57 -0500

    fixing up tests with new dbs

--- src/main/cpp/test/mpc/secure_tpch_test.cpp
+++ src/main/cpp/test/mpc/secure_tpch_test.cpp
@@ -19,8 +19,6 @@ using namespace vaultdb;
 // JMR: This is db dependendant
 // TODO: either add DBs to repo to make this reproducible or find a better way to test
 
-#define DIAGNOSE 1
-#define TRUNCATE 0
 
 
 DEFINE_int32(party, 1, "party for EMP execution");
@@ -34,6 +32,8 @@ protected:
 
     // depends on truncate-tpch-set.sql
     void runTest(const int &test_id, const string & test_name, const SortDefinition &expected_sort, const string &db_name);
+    string  generateExpectedOutputQuery(const int & test_id,  const SortDefinition &expected_sort,   const string &db_name);
+
     int input_tuple_limit_ = 1000;
 
 };
@@ -42,38 +42,34 @@ protected:
 // This isn't relevant to the parser so work on this elsewhere.
 void
 SecureTpcHTest::runTest(const int &test_id, const string & test_name, const SortDefinition &expected_sort, const string &db_name) {
-    string query = tpch_queries[test_id];
+    string expected_query = generateExpectedOutputQuery(test_id, expected_sort, db_name);
+
     string party_name = FLAGS_party == emp::ALICE ? "alice" : "bob";
-    string local_db_name = db_name;
-    boost::replace_first(local_db_name, "unioned", party_name.c_str());
+    string local_db = db_name;
+    boost::replace_first(local_db, "unioned", party_name.c_str());
 
-    if(TRUNCATE) {
-        query = truncated_tpch_queries[test_id];
-        boost::replace_all(query, "$LIMIT", std::to_string(input_tuple_limit_));
-    }
-
-    // supercedes truncate flag
-    if(test_id == 18 && test_name == "q18-truncated") {
-        query = truncated_tpch_queries[18]; // 18++
-        boost::replace_all(query, "$LIMIT", std::to_string(input_tuple_limit_));
-
-    }
-
-
-
-    shared_ptr<PlainTable> expected = DataUtilities::getExpectedResults(db_name, query, false, 0);
+    std::cout << "Querying " << db_name << " with query: \n" << expected_query << '\n';
+    shared_ptr<PlainTable> expected = DataUtilities::getExpectedResults(db_name, expected_query, false, 0);
     expected->setSortOrder(expected_sort);
 
-//    // JMR, this Q5 + Q18 exception is a temporary hack.
-//    TODO: fix this with better input data and adding the input DBs to the repo
-//    if(DIAGNOSE == 1) {
-//        ASSERT_GT(expected->getTupleCount(),  0);
-//    }
+    // tpch_alice_150 row counts:
+    // SELECT l_returnflag, l_linestatus, COUNT(*) FROM (SELECT * FROM lineitem ORDER BY  l_orderkey, l_linenumber LIMIT 1000 ) l  WHERE l_shipdate <= date '1998-08-03' GROUP BY  l_returnflag, l_linestatus;
+    //  l_returnflag | l_linestatus | count
+    //--------------+--------------+-------
+    // R            | F            |   222
+    // A            | F            |   244
+    // N            | F            |    10
+    // N            | O            |   498
+
+    // bob:
+
+    std::cout << "Expected query answer: " << *expected << '\n';
 
 
-    int limit = (TRUNCATE) ? input_tuple_limit_ : -1;
-    PlanParser<emp::Bit> parser(local_db_name, test_name, netio_, FLAGS_party, limit);
+    PlanParser<emp::Bit> parser(local_db, test_name, netio_, FLAGS_party, input_tuple_limit_);
     shared_ptr<SecureOperator> root = parser.getRoot();
+
+    std::cout << "Query tree: \n" << root->printTree() << '\n';
 
     shared_ptr<PlainTable> observed = root->run()->reveal();
     observed = DataUtilities::removeDummies(observed);
@@ -82,14 +78,30 @@ SecureTpcHTest::runTest(const int &test_id, const string & test_name, const Sort
 
 }
 
-//
-//// Passes, 1.5 mins @ DIAGNOSE
-TEST_F(SecureTpcHTest, tpch_q1) {
-    SortDefinition expected_sort = DataUtilities::getDefaultSortDefinition(2);
-    runTest(1, "q1", expected_sort, "tpch_unioned_50");
+string
+SecureTpcHTest::generateExpectedOutputQuery(const int &test_id, const SortDefinition &expected_sort, const string &db_name) {
+    string alice_db = db_name;
+    string bob_db = db_name;
+    boost::replace_first(alice_db, "unioned", "alice");
+    boost::replace_first(bob_db, "unioned", "bob");
+
+    string query = tpch_queries[test_id];
+
+    if(input_tuple_limit_ > 0) {
+        query = truncated_tpch_queries[test_id];
+        boost::replace_all(query, "$LIMIT", std::to_string(input_tuple_limit_));
+        boost::replace_all(query, "$ALICE_DB", alice_db);
+        boost::replace_all(query, "$BOB_DB", bob_db);
+    }
+
+    return query;
 }
 
-// Passes, 4+ mins @ DIAGNOSE
+TEST_F(SecureTpcHTest, tpch_q1) {
+    SortDefinition expected_sort = DataUtilities::getDefaultSortDefinition(2);
+    runTest(1, "q1", expected_sort, unioned_db_);
+}
+
 TEST_F(SecureTpcHTest, tpch_q3) {
 
     // dummy_tag (-1), 1 DESC, 2 ASC
@@ -97,55 +109,49 @@ TEST_F(SecureTpcHTest, tpch_q3) {
     SortDefinition expected_sort{ColumnSort(-1, SortDirection::ASCENDING),
                                  ColumnSort(1, SortDirection::DESCENDING),
                                  ColumnSort(2, SortDirection::ASCENDING)};
-    runTest(3, "q3", expected_sort, "tpch_unioned_50");
+    runTest(3, "q3", expected_sort, unioned_db_);
 }
 
 
 
 
-// runs for 2 hours in MPC on tpch_unioned_250
- // now faster since we are hand-picking the keys to admit
-TEST_F(SecureTpcHTest, tpch_q5) {
-    SortDefinition  expected_sort{ColumnSort(1, SortDirection::DESCENDING)};
-    // to get non-empty results, run with tpch_unioned_1000 - runs for ~40 mins in plaintext
-    // JMR: ran for 12+ hours in MPC before killing it.
-    // reduced the runtime by adding a where clause at the bottom of each SQL query
-    //string db_name = (DIAGNOSE == 1) ? "tpch_unioned_250" : "tpch_unioned_50";
-    string db_name = "tpch_unioned_50";
-    runTest(5, "q5", expected_sort, db_name);
-
-}
-
-
-// runs for ~9 minutes
-TEST_F(SecureTpcHTest, tpch_q8) {
-    SortDefinition expected_sort = DataUtilities::getDefaultSortDefinition(1);
-    runTest(8, "q8", expected_sort, "tpch_unioned_1000");
-}
-
-
-// passed, runs in 15 mins
-TEST_F(SecureTpcHTest, tpch_q9) {
-    // $0 ASC, $1 DESC
-    SortDefinition  expected_sort{ColumnSort(0, SortDirection::ASCENDING), ColumnSort(1, SortDirection::DESCENDING)};
-    runTest(9, "q9", expected_sort, "tpch_unioned_100");
-
-}
-
-// passed, runs < 1 minute
-TEST_F(SecureTpcHTest, tpch_q18) {
-    // -1 ASC, $4 DESC, $3 ASC
-    SortDefinition expected_sort{ColumnSort(-1, SortDirection::ASCENDING),
-                                 ColumnSort(4, SortDirection::DESCENDING),
-                                 ColumnSort(3, SortDirection::ASCENDING)};
-
-    string db_name = (DIAGNOSE == 1) ? "tpch_unioned" : "tpch_unioned_50";
-    string test_name = (DIAGNOSE == 1) ? "q18-truncated" : "q18";
-
-    runTest(18, test_name, expected_sort, db_name);
-}
-
-
+//TEST_F(SecureTpcHTest, tpch_q5) {
+//    SortDefinition  expected_sort{ColumnSort(1, SortDirection::DESCENDING)};
+//    // to get non-empty results, run with tpch_unioned_1000 - runs for ~40 mins in plaintext
+//    // JMR: ran for 12+ hours in MPC before killing it.
+//    // reduced the runtime by adding a where clause at the bottom of each SQL query
+//    runTest(5, "q5", expected_sort, unioned_db_);
+//
+//}
+//
+//
+//// runs for ~9 minutes
+//TEST_F(SecureTpcHTest, tpch_q8) {
+//    SortDefinition expected_sort = DataUtilities::getDefaultSortDefinition(1);
+//    runTest(8, "q8", expected_sort, unioned_db_);
+//}
+//
+//
+//// passed, runs in 15 mins
+//TEST_F(SecureTpcHTest, tpch_q9) {
+//    // $0 ASC, $1 DESC
+//    SortDefinition  expected_sort{ColumnSort(0, SortDirection::ASCENDING), ColumnSort(1, SortDirection::DESCENDING)};
+//    runTest(9, "q9", expected_sort, unioned_db_);
+//
+//}
+//
+//// passed, runs < 1 minute
+//TEST_F(SecureTpcHTest, tpch_q18) {
+//    // -1 ASC, $4 DESC, $3 ASC
+//    SortDefinition expected_sort{ColumnSort(-1, SortDirection::ASCENDING),
+//                                 ColumnSort(4, SortDirection::DESCENDING),
+//                                 ColumnSort(3, SortDirection::ASCENDING)};
+//
+//
+//    runTest(18, "q18", expected_sort, unioned_db_);
+//}
+//
+//
 
 
 int main(int argc, char **argv) {
