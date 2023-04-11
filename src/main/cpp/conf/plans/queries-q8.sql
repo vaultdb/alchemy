fcae2d6d6d2a59a4d82bea525afec8c400ed5898
Jennie Rogers <jennie@northwestern.edu>
2023-04-11 12:52:35 -0500

    working on making secure tpc-h faster with non-empty results

--- src/main/cpp/conf/plans/queries-q8.sql
+++ src/main/cpp/conf/plans/queries-q8.sql
@@ -2,7 +2,7 @@
 SELECT c_nationkey, c_custkey
 FROM customer c JOIN nation n1 ON c_nationkey = n_nationkey
                 JOIN region ON r_regionkey = n_regionkey
-WHERE r_name = 'MIDDLE EAST'
+WHERE r_name = 'AFRICA'
 ORDER BY c_custkey;
 -- 1, collation: (1 ASC, 0 ASC)
 SELECT o_orderkey, o_custkey, o_orderyear::INT AS o_year, NOT (o_orderdate >= date '1995-01-01' and o_orderdate < date '1996-12-31') dummy_tag
@@ -11,9 +11,9 @@ ORDER BY o_custkey, o_orderkey;
 -- 3, collation: (1 ASC)
 SELECT l_partkey, l_orderkey, l_suppkey, l_extendedprice * (1.0 - l_discount) volume
 FROM part JOIN lineitem ON p_partkey = l_partkey
-WHERE p_type = 'ECONOMY ANODIZED TIN'
+WHERE p_type = 'LARGE ANODIZED STEEL'
 ORDER BY l_orderkey, l_linenumber;
 -- 4, collation: (0 ASC)
-SELECT s_suppkey, CASE WHEN n_name = 'EGYPT' THEN 1.0 ELSE 0.0 END AS nation_check
+SELECT s_suppkey, CASE WHEN n_name = 'KENYA' THEN 1.0 ELSE 0.0 END AS nation_check
 FROM supplier JOIN nation ON n_nationkey = s_nationkey
 ORDER BY s_suppkey;
