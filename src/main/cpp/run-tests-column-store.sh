8b7dd53a4c7e03a2d26e9faaa470f695b0ae771b
VaultDB User <vaultdb@northwestern.edu>
2023-06-09 23:08:38 +0000

    bug fix in runner script

--- src/main/cpp/run-tests-column-store.sh
+++ src/main/cpp/run-tests-column-store.sh
@@ -28,25 +28,25 @@ bash  test/support/load-generated-data.sh 100
 ./bin/plan_parser_test --storage=column
 ./bin/tpch_test --storage=column
 
-bash run.sh emp_test 
-bash run.sh secure_field_expression_test  
-bash run.sh emp_table_test 
-bash run.sh secure_filter_test 
-bash run.sh secure_sort_test 
-bash run.sh secure_basic_join_test 
-bash run.sh secure_keyed_join_test 
-bash run.sh secure_scalar_aggregate_test 
-bash run.sh secure_group_by_aggregate_test 
-bash run.sh enrich_test
-bash run.sh secure_tpch_test
+bash run-column-store.sh emp_test 
+bash run-column-store.sh secure_field_expression_test  
+bash run-column-store.sh emp_table_test 
+bash run-column-store.sh secure_filter_test 
+bash run-column-store.sh secure_sort_test 
+bash run-column-store.sh secure_basic_join_test 
+bash run-column-store.sh secure_keyed_join_test 
+bash run-column-store.sh secure_scalar_aggregate_test 
+bash run-column-store.sh secure_group_by_aggregate_test 
+bash run-column-store.sh enrich_test
+bash run-column-store.sh secure_tpch_test
 
 # ZK Tests -- these are optional
-#bash run.sh zk_test
-#bash run.sh zk_filter_test
-#bash run.sh  zk_basic_join_test
-#bash run.sh zk_keyed_join_test
-#bash run.sh  zk_scalar_aggregate_test
-#bash run.sh zk_group_by_aggregate_test
-#bash run.sh zk_sort_test
-#bash run.sh zk_tpch_test
+#bash run-column-store.sh zk_test
+#bash run-column-store.sh zk_filter_test
+#bash run-column-store.sh  zk_basic_join_test
+#bash run-column-store.sh zk_keyed_join_test
+#bash run-column-store.sh  zk_scalar_aggregate_test
+#bash run-column-store.sh zk_group_by_aggregate_test
+#bash run-column-store.sh zk_sort_test
+#bash run-column-store.sh zk_tpch_test
 
