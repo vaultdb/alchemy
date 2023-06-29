5e2404d50f24016176dd85012538c1cd8e43d7af
Jennie Rogers <jennie@northwestern.edu>
2023-06-28 19:43:02 -0500

    protocol init in plain base test

--- src/main/cpp/test/plain/plain_base_test.cpp
+++ src/main/cpp/test/plain/plain_base_test.cpp
@@ -2,6 +2,17 @@
 #include <emp-tool/emp-tool.h>
 #include <util/system_configuration.h>
 #include "util/field_utilities.h"
+#include "util/emp_manager/outsourced_mpc_manager.h"
+#include "util/emp_manager/sh2pc_manager.h"
+
+#if __has_include("emp-rescu/emp-rescu.h")
+static EmpMode _emp_mode_ = EmpMode::OUTSOURCED;
+#elif  __has_include("emp-sh2pc/emp-sh2pc.h")
+static EmpMode _emp_mode_ = EmpMode::SH2PC;
+#else
+    static EmpMode _emp_mode_ = EmpMode::PLAIN;
+#endif
+
 
 void PlainBaseTest::SetUp()  {
     assert(FLAGS_storage == "row" || FLAGS_storage == "column");
@@ -9,16 +20,22 @@ void PlainBaseTest::SetUp()  {
 
     std::cout << "Received storage flag of " << FLAGS_storage << '\n';
 
-    setup_plain_prot(false, "");
-    //Logger::setup(); // write to console
-    //Logger::setup("vaultdb");
+    if(_emp_mode_ == EmpMode::OUTSOURCED) {
+        manager_ = new OutsourcedMpcManager();
+    } else if(_emp_mode_ == EmpMode::SH2PC) {
+        manager_ = new SH2PCManager();
+    } else {
+        throw std::runtime_error("Unsupported EMP mode");
+    }
+
     current_working_directory_ = Utilities::getCurrentWorkingDirectory();
     SystemConfiguration &s = SystemConfiguration::getInstance();
     BitPackingMetadata md = FieldUtilities::getBitPackingMetadata(db_name_);
     s.initialize(db_name_, md, storage_model_);
+    s.emp_manager_ = manager_;
 };
 
 void PlainBaseTest::TearDown() {
+    // teardown covered in SystemConfiguration destructor
 
-    finalize_plain_prot();
-};
+}
