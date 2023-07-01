6ce05e72ee0c93fb42b21c2e6ddb8e9be39ccf76
donghyun-sohn <bulelion37@naver.com>
2023-07-01 22:58:42 +0000

    Add Baseline Script

---
+++ src/main/cpp/run-baseline-comparison-1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+executable="./baseline_comparison_test"
+PARTY=1
+
+echo "Running tests with test=baseline"
+$executable --party=$PARTY -test=baseline > baseline_comparison_test${PARTY}_baseline.log
+
+echo "Running tests with test=handcode"
+$executable --party=$PARTY --alice_host=129.105.61.182 --test=handcode > baseline_comparison_test${PARTY}_handcode.log
