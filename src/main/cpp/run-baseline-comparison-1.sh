87418988832e0b0b724d5576be1ea8b00b28a9c9
Donghyun Sohn <44919498+donghyun-sohn@users.noreply.github.com>
2023-07-01 22:45:20 -0500

    Update run-baseline-comparison-1.sh

--- src/main/cpp/run-baseline-comparison-1.sh
+++ src/main/cpp/run-baseline-comparison-1.sh
@@ -4,7 +4,7 @@ executable="./bin/baseline_comparison_test"
 PARTY=1
 
 echo "Running tests with test=baseline"
-$executable --party=$PARTY -test=baseline > baseline_comparison_test${PARTY}_baseline.log
+$executable --party=$PARTY --alice_host=129.105.61.182 --test=baseline > baseline_comparison_test${PARTY}_baseline.log
 
 echo "Running tests with test=handcode"
 $executable --party=$PARTY --alice_host=129.105.61.182 --test=handcode > baseline_comparison_test${PARTY}_handcode.log
