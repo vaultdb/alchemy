fe324ea732f8ca0195cc4c6fdacc1458e36fad78
Donghyun Sohn <bulelion37@naver.com>
2023-07-05 21:52:49 +0000

    SecureAggregateComparisonTest passed

---
+++ src/main/cpp/run-aggregate-comparison-1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+executable="./bin/secure_aggregate_comparison_test"
+PARTY=1
+
+# Get today's date
+date=$(date +'%Y-%m-%d')
+
+# Define the cutoff values
+cutoff_values=(10 100 1000)
+
+log_file="AggregateComparison_${date}.log"
+
+for cutoff in "${cutoff_values[@]}"
+do
+    echo "Running tests with test=baseline" >> "$log_file"
+    $executable --party=$PARTY --cutoff=$cutoff --bitpacking="packed" >> "$log_file"
+
+    echo "Running tests with test=handcode" >> "$log_file"
+    $executable --party=$PARTY --cutoff=$cutoff --bitpacking="packed" >> "$log_file"
+
+    echo "Running tests with test=baseline" >> "$log_file"
+    $executable --party=$PARTY --cutoff=$cutoff --bitpacking="non" >> "$log_file"
+
+    echo "Running tests with test=handcode" >> "$log_file"
+    $executable --party=$PARTY --cutoff=$cutoff --bitpacking="non" >> "$log_file"
+done
