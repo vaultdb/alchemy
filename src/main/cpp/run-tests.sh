57cd7772cc9f732136380ae565fe4a5268a53591
Jennie Rogers <jennie@northwestern.edu>
2023-08-01 18:24:43 -0500

    re-adding pilot to test script

--- src/main/cpp/run-tests.sh
+++ src/main/cpp/run-tests.sh
@@ -11,8 +11,9 @@ bash  test/support/load-generated-data.sh 100
 
 # optional:
 #bash run-pilot-test.sh
-#./bin/run_data_partner -h 127.0.0.1 -P 54321 --party=1 -d enrich_htn_alice -p pilot/secret_shares/tables/chi_partial_counts.alice -r pilot/test/output/chi-patient-multisite.alice -y all -s &
-#./bin/run_data_partner -h 127.0.0.1 -P 54321 --party=2 -d enrich_htn_bob -p pilot/secret_shares/tables/chi_partial_counts.bob -r pilot/test/output/chi-patient-multisite.bob -y all -s
+./bin/run_data_partner -h 127.0.0.1 -P 54321 --party=1 -d enrich_htn_alice -p pilot/secret_shares/tables/chi_partial_counts.alice -r pilot/test/output/chi-patient-multisite.alice -y all -s &
+./bin/run_data_partner -h 127.0.0.1 -P 54321 --party=2 -d enrich_htn_bob -p pilot/secret_shares/tables/chi_partial_counts.bob -r pilot/test/output/chi-patient-multisite.bob -y all -s
+./bin/assemble_secret_shares pilot/secret_shares/xor pilot/secret_shares/revealed
 
 
 
