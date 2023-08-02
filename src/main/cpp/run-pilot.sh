a5b05f0ef4fb8e164e69ecd7dc5fce22a92aef02
Jennie Rogers <jennie@northwestern.edu>
2023-08-02 15:18:49 -0500

    adding debug info to pilot script

--- src/main/cpp/run-pilot.sh
+++ src/main/cpp/run-pilot.sh
@@ -19,6 +19,7 @@ mkdir -p pilot/secret_shares/tables
 ./bin/run_data_partner -h 127.0.0.1 -P 54321 --party=1 -d enrich_htn_alice -p pilot/secret_shares/tables/chi_partial_counts.alice -r pilot/test/output/chi-patient-multisite.alice -y all -s &
 ./bin/run_data_partner -h 127.0.0.1 -P 54321 --party=2 -d enrich_htn_bob -p pilot/secret_shares/tables/chi_partial_counts.bob -r pilot/test/output/chi-patient-multisite.bob -y all -s
 
+echo "Assembling secret shares"
 
 ./bin/assemble_secret_shares pilot/secret_shares/xor pilot/secret_shares/revealed
 
