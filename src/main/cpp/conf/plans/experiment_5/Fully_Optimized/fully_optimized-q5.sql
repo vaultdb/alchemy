8c1d9e8324106d3e6254d3a0a5ec29d9a6b8a19c
Jennie Rogers <jennie@northwestern.edu>
2023-08-22 18:11:25 -0500

    optimizing placement of n_name in experiment 5

--- src/main/cpp/conf/plans/experiment_5/Fully_Optimized/fully_optimized-q5.sql
+++ src/main/cpp/conf/plans/experiment_5/Fully_Optimized/fully_optimized-q5.sql
@@ -1,13 +1,9 @@
 -- 0, collation: (0 ASC)
-SELECT t2.c_custkey, t2.c_nationkey
-FROM (SELECT r_regionkey, r_name
-FROM region
-WHERE r_name = 'EUROPE') AS t0
-INNER JOIN (SELECT n_nationkey, n_name, n_regionkey
-FROM nation) AS t1 ON t0.r_regionkey = t1.n_regionkey
-INNER JOIN (SELECT c_custkey, c_nationkey
-FROM customer) AS t2 ON t1.n_nationkey = t2.c_nationkey
-ORDER BY t2.c_custkey, t2.c_nationkey
+SELECT c_custkey, c_nationkey
+FROM customer JOIN nation ON c_nationkey = n_nationkey
+JOIN region ON n_regionkey = r_regionkey
+WHERE r_name = 'EUROPE'
+ORDER BY c_custkey, c_nationkey
 -- 1, collation: (1 ASC)
 SELECT o_orderkey, o_custkey, NOT (o_orderdate >= DATE '1993-01-01' AND o_orderdate < DATE '1994-01-01') AS dummy_tag
 FROM orders
@@ -17,12 +13,8 @@ SELECT l_orderkey, l_suppkey, l_extendedprice * (1 - l_discount) AS revenue
 FROM lineitem
 ORDER BY l_orderkey, revenue DESC
 -- 7
-SELECT t2.s_suppkey, t2.s_nationkey, n_name
-FROM (SELECT r_regionkey, r_name
-FROM region
-WHERE r_name = 'EUROPE') AS t0
-INNER JOIN (SELECT n_nationkey, n_regionkey, n_name
-FROM nation) AS t1 ON t0.r_regionkey = t1.n_regionkey
-INNER JOIN (SELECT s_suppkey, s_nationkey
-FROM supplier) AS t2 ON t1.n_nationkey = t2.s_nationkey
-ORDER BY t2.s_nationkey, t2.s_suppkey
+SELECT s_suppkey, s_nationkey, n_name
+FROM supplier JOIN nation ON s_nationkey = n_nationkey
+   JOIN region ON n_regionkey = r_regionkey
+WHERE r_name = 'EUROPE'
+ORDER BY s_nationkey, s_suppkey
