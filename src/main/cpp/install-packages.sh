ad30bbdd88d553c5d2ddee46a41d11b1cacb8372
Jennie Rogers <jennie@northwestern.edu>
2023-08-22 18:11:07 -0500

    optimizing placement of n_name in experiment 5

--- src/main/cpp/install-packages.sh
+++
@@ -1,5 +0,0 @@
-echo -n Password:
-read -s PASSWORD
-#PASSWORD="" #password"
-echo $PASSWORD | sudo -S apt-get update
-echo $PASSWORD | sudo -S apt-get -y install build-essential autoconf libtool pkg-config cmake git clang libgoogle-perftools-dev ocaml ocamlbuild libssl-dev libgmp-dev python libboost-dev libboost-{chrono,log,program-options,date-time,thread,system,filesystem,regex,test}-dev libpq-dev
