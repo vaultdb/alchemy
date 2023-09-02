45507d54962a053005279ef32bd86e9ddb5dd049
Jennie Rogers <jennie@northwestern.edu>
2023-09-01 23:07:09 -0500

    re-adding ZK to test suite

--- src/main/cpp/run-tests.sh
+++ src/main/cpp/run-tests.sh
@@ -9,7 +9,6 @@ bash pilot/test/generate-and-load-test-data.sh 1000
 # regenerate test data
 bash  test/support/load-generated-data.sh 100
 
-# optional:
 #bash run-pilot-test.sh
 ./bin/run_data_partner -h 127.0.0.1 -P 54321 --party=1 -d enrich_htn_alice -p pilot/secret_shares/tables/chi_partial_counts.alice -r pilot/test/output/chi-patient-multisite.alice -y all -s &
 ./bin/run_data_partner -h 127.0.0.1 -P 54321 --party=2 -d enrich_htn_bob -p pilot/secret_shares/tables/chi_partial_counts.bob -r pilot/test/output/chi-patient-multisite.bob -y all -s
@@ -28,5 +27,5 @@ bash run-mpc-tests.sh
 #bash run.sh  zk_scalar_aggregate_test
 #bash run.sh zk_sort_merge_aggregate_test
 #bash run.sh zk_sort_test
-#bash run.sh zk_tpch_test
+bash run.sh zk_tpch_test
 
