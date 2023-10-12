20134b2bbf9c60eebf303dbf6a44c56d241a6fa9
VaultDB User <vaultdb@northwestern.edu>
2023-10-12 23:52:47 +0000

    adding sleep in col store script

--- src/main/cpp/run-column-store.sh
+++ src/main/cpp/run-column-store.sh
@@ -2,5 +2,6 @@
 killall -q $1
 
 ./bin/$1 --flagfile=flagfiles/alice.flags --storage="column" &
-./bin/$1 --flagfile=flagfiles/bob.flags --storage="column"
 sleep 0.5
+./bin/$1 --flagfile=flagfiles/bob.flags --storage="column"
+sleep 1
