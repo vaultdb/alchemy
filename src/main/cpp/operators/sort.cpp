194b7b4d44ae5c48af79ec3e5455f222c0547b94
Xiling Li <xilingli@dhcp-10-105-236-43.wireless.northwestern.private>
2023-10-12 10:23:33 -0500

    update bool Sort<B>::swapTuples for col store in the plain sort execution

--- src/main/cpp/operators/sort.cpp
+++ src/main/cpp/operators/sort.cpp
@@ -3,6 +3,7 @@
 #include <query_table/plain_tuple.h>
 #include <util/data_utilities.h>
 #include <query_table/row_table.h>
+#include <query_table/column_table.h>
 #include <operators/support/normalize_fields.h>
 
 
@@ -153,28 +154,31 @@ Bit Sort<B>::swapTuples(const QueryTable<Bit> *table, const int &lhs_idx, const 
 template<typename B>
 bool Sort<B>::swapTuples(const QueryTable<bool> *table, const int &lhs_idx, const int &rhs_idx,
                                    const bool &dir, const int &sort_key_width_bits) {
-    auto row_table = (RowTable<bool> *) table;
 
+    bool is_row_store = SystemConfiguration::getInstance().storageModel() == StorageModel::ROW_STORE;
 
-    int8_t *lhs =  (row_table->tuple_data_.data() + row_table->tuple_size_bytes_ * lhs_idx);
-    int8_t *rhs =  (row_table->tuple_data_.data() + row_table->tuple_size_bytes_ * rhs_idx);
+    QueryTuple<bool> lhs_tuple = is_row_store ? ((RowTable<bool> *) table)->getPlainTuple(lhs_idx) : ((ColumnTable<bool> *) table)->getPlainTuple(lhs_idx);
+    QueryTuple<bool> rhs_tuple = is_row_store ? ((RowTable<bool> *) table)->getPlainTuple(rhs_idx) : ((ColumnTable<bool> *) table)->getPlainTuple(rhs_idx);
+
+    int8_t *lhs = lhs_tuple.getData();
+    int8_t *rhs = rhs_tuple.getData();
     int byte_cnt = sort_key_width_bits / 8;
 
     // TODO: streamline this with memcmp over reversed keys
     bool lhs_gt = false;
-    for(int i = byte_cnt - 1; i >= 0; --i) {
+    for (int i = byte_cnt - 1; i >= 0; --i) {
         auto l = (unsigned int) lhs[i];
         auto r = (unsigned int) rhs[i];
 
-        if(l == r) continue;
-        if(l > r) {
+        if (l == r) continue;
+        if (l > r) {
             lhs_gt = true;
             break;
         }
         break;
     }
 
-    return  (lhs_gt == dir);
+    return (lhs_gt == dir);
 }
 
 
