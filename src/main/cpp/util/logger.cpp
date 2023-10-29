1f12f1906a4f2c172048f77092fcf458ea26b79b
Jennie Rogers <jennie@northwestern.edu>
2023-10-29 17:34:56 -0500

    reverting for gflags

--- src/main/cpp/util/logger.cpp
+++ src/main/cpp/util/logger.cpp
@@ -7,14 +7,14 @@ using namespace Logging;
 
 void Logger::write(const std::string & msg, Level request_level_ )
 {
-    if((int)request_level_ >= FLAGS_log_level) {
-        std::cout << msg << '\n';
-        if(!FLAGS_logfile.empty()) {
-            std::ofstream logfile;
-            logfile.open(FLAGS_logfile, std::ios::out | std::ios::app);
-            logfile << msg << "\n";
-        }
-    }
+	if((int)request_level_ >= FLAGS_log_level) {		
+		std::cout << msg << '\n';
+		if(!FLAGS_logfile.empty()) {
+			std::ofstream logfile;
+			logfile.open(FLAGS_logfile, std::ios::out | std::ios::app);
+			logfile << msg << "\n";
+		}
+	}
 }
 //
 //void Logger::setup(const std::string & logfile_prefix) {
