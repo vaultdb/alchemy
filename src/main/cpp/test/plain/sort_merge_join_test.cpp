0d6ae3f4067482484e4bea997f04cc54fa162bd4
Jennie Rogers <jennie@northwestern.edu>
2023-11-15 11:44:38 -0600

    added sort merge join test

--- src/main/cpp/test/plain/sort_merge_join_test.cpp
+++ src/main/cpp/test/plain/sort_merge_join_test.cpp
@@ -7,7 +7,9 @@
 #include <util/data_utilities.h>
 
 #include <test/support/tpch_queries.h>
-
+#include <operators/sql_input.h>
+#include <operators/sort_merge_join.h>
+#include "util/field_utilities.h"
 
 
 DEFINE_int32(cutoff, 100, "limit clause for queries");
@@ -18,38 +20,62 @@ class SortMergeJoinTest :  public PlainBaseTest  {
 
 protected:
  
-
-    const std::string customer_sql_ = "SELECT c_custkey, c_mktsegment <> 'HOUSEHOLD' c_dummy \n"
-                                      "FROM customer  \n"
-                                      "WHERE c_custkey <= " + std::to_string(FLAGS_cutoff) +
-                                      " ORDER BY c_custkey";
-
-
-    const std::string orders_cust_sql_ = "SELECT o_orderkey, o_custkey, o_orderdate, o_shippriority, o_orderdate >= date '1995-03-25' o_dummy \n"
-                                    "FROM orders \n"
-                                    "WHERE o_custkey <=  " + std::to_string(FLAGS_cutoff) +
-                                    " ORDER BY o_custkey, o_orderkey";
-
-    const std::string orders_lineitem_sql_ = "SELECT o_orderkey, o_custkey, o_orderdate, o_shippriority, o_orderdate >= date '1995-03-25' o_dummy \n"
-                                         "FROM orders \n"
-                                         "WHERE o_custkey <=  " + std::to_string(FLAGS_cutoff) +
-                                         " ORDER BY o_orderkey";
+// simulating TPC-H Q5
+    const std::string customer_sql_ = "SELECT c_custkey, c_nationkey, r_name = 'EUROPE' c_dummy \n"
+                                      "FROM customer  JOIN nation ON c_nationkey = n_nationkey"
+                                      "     JOIN region ON r_regionkey = n_regionkey \n"
+                                      " ORDER BY c_custkey "
+                                      "LIMIT " + std::to_string(FLAGS_cutoff);
 
 
-    const std::string lineitem_sql_ = "SELECT  l_orderkey, l_extendedprice * (1 - l_discount) revenue, l_shipdate <= date '1995-03-25' l_dummy \n"
-                                      "FROM lineitem \n"
-                                      "WHERE l_orderkey IN (SELECT o_orderkey FROM orders where o_custkey <= " + std::to_string(FLAGS_cutoff) + ")  \n"
-                                     " ORDER BY l_orderkey, revenue, l_dummy ";
+    const std::string supplier_sql_ = "SELECT s_suppkey, s_nationkey, r_name = 'EUROPE' s_dummy \n"
+                                      "FROM supplier  JOIN nation ON c_nationkey = n_nationkey"
+                                      "     JOIN region ON r_regionkey = n_regionkey \n"
+                                      " ORDER BY s_suppkey "
+                                      "LIMIT " + std::to_string(FLAGS_cutoff);
 
     SortDefinition customer_sort_ = DataUtilities::getDefaultSortDefinition(1);
-    SortDefinition orders_cust_sort_{ColumnSort (1, SortDirection::ASCENDING), ColumnSort (0, SortDirection::ASCENDING)};
-    SortDefinition orders_lineitem_sort_{ColumnSort (0, SortDirection::ASCENDING)};
-    SortDefinition lineitem_sort_ = DataUtilities::getDefaultSortDefinition(2);
+    SortDefinition supplier_sort_ = DataUtilities::getDefaultSortDefinition(1);
+    // TODO: check this - will depend on operator implementation
+    SortDefinition customer_supplir_sort_ = {{0, SortDirection::ASCENDING}, {2, SortDirection::ASCENDING}};
 
-	void runTest(const int &test_id, const string & test_name, const SortDefinition &expected_sort, const string &db_name) {
-	}
+
 };
 
+
+TEST_F(SortMergeJoinTest, test_tpch_q3_customer_orders) {
+
+    std::string expected_sql = "WITH customer_cte AS (" + customer_sql_ + "), "
+                                     "supplier_cte AS (" + supplier_sql_ + ") "
+                               "SELECT * "
+                               "FROM  customer_cte JOIN supplier_cte ON c_nationkey = s_nationkey "
+                               "WHERE NOT c_dummy AND NOT s_dummy "
+                               "ORDER BY c_custkey ";
+
+
+    PlainTable *expected = DataUtilities::getQueryResults(db_name_, expected_sql, false);
+
+    auto customer_input = new SqlInput(db_name_, customer_sql_, true, customer_sort_);
+    auto supplier_input = new SqlInput(db_name_, supplier_sql_, true, supplier_sort_);
+
+    // join output schema: (customer, supplier)
+    // c_custkey, c_nationkey, s_suppkey, s_nationkey
+    Expression<bool> *predicate = FieldUtilities::getEqualityPredicate<bool>(customer_input, 0, supplier_input, 2);
+
+    auto join = new SortMergeJoin<bool>(customer_input, supplier_input,  predicate);
+    auto joined = join->run();
+    SortDefinition sort_def = DataUtilities::getDefaultSortDefinition(2);
+    Sort<bool> sorter(joined, sort_def);
+    PlainTable *observed = sorter.run();
+    expected->order_by_ = observed->order_by_;
+    DataUtilities::removeDummies(observed);
+
+    ASSERT_EQ(*expected, *observed);
+
+    delete expected;
+
+
+}
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     gflags::ParseCommandLineFlags(&argc, &argv, false);
