9046c4032724f528a84ae00500879c8c088a6ca9
Jennie Rogers <jennie@northwestern.edu>
2023-12-12 15:18:05 -0600

    cleaning up ompc run scripts

--- src/main/cpp/scripts/run-ompc.sh
+++ src/main/cpp/scripts/run-ompc.sh
@@ -11,10 +11,10 @@ fi
 
 [ -d log ] || mkdir log
 
-./bin/$1 --party=10086   | tee  'log/'$1'-p10086.log' &
+./bin/$1  --flagfile=flagfiles/tp.flags  | tee  'log/'$1'-p10086.log' &
 sleep 0.05
-./bin/$1 --party=1 > 'log/'$1'-p1.log'  &
+./bin/$1 --flagfile=flagfiles/alice.flags > 'log/'$1'-p1.log'  &
  sleep 0.05
- ./bin/$1 --party=2  > 'log/'$1'-p2.log'    &
+ ./bin/$1  --flagfile=flagfiles/bob.flags > 'log/'$1'-p2.log'    &
  sleep 0.05
-./bin/$1 --party=3 > 'log/'$1'-p3.log'
+./bin/$1   --flagfile=flagfiles/chi.flags > 'log/'$1'-p3.log'
