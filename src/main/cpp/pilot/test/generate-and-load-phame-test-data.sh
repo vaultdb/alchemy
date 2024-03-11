5cbcac8138bb3c4dc58a8d42e6894eca2c50ac46
Jennie Rogers <jennie@northwestern.edu>
2024-03-11 14:16:56 -0500

    added secret sharing logic for phame test pipeline

--- src/main/cpp/pilot/test/generate-and-load-phame-test-data.sh
+++ src/main/cpp/pilot/test/generate-and-load-phame-test-data.sh
@@ -11,13 +11,14 @@ DB_NAME='phame_unioned'
 
 mkdir -p pilot/test/input
 mkdir -p pilot/test/output
-mkdir -p pilot/secret-shares/input/0
-mkdir pilot/secret-shares/input/1
-mkdir pilot/secret-shares/input/2
-mkdir pilot/secret-shares/input/3
+mkdir -p pilot/secret_shares/input/0
+mkdir pilot/secret_shares/input/1
+mkdir pilot/secret_shares/input/2
+mkdir pilot/secret_shares/input/3
+
 
 #clear out old data
-rm pilot/secret-shares/input/*/*
+rm pilot/secret_shares/input/*/*
 
 make generate_phame_data_n_parties
 ./bin/generate_phame_data_n_parties pilot/test/input $HOST_COUNT $TUPLE_COUNT
@@ -35,12 +36,31 @@ psql $DB_NAME <  pilot/test/phame/load-generated-data.sql
 
 # create schema files for each test CSV
 for((i = 0; i < $HOST_COUNT; i++)); do
-  echo '(patid:int32, dx_diabetes:bool, dx_hypertension:bool, dx_cervical_cancer:bool, dx_breast_cancer:bool, dx_lung_cancer:bool, dx_colorectal_cancer:bool)' > pilot/secret-shares/input/$i/phame_diagnosis_rollup.schema
-  echo '(desc:varchar(42), count:int64, site_id:int32)' > pilot/secret-shares/input/$i/phame_cohort_counts.schema
+  echo '(age_cat:char(1), gender:char(1), race:char(1), ethnicity:char(1), zip:char(5), payer_primary:char(1), payer_secondary:char(1), patient_cnt:int64, diabetes_cnt:int64, hypertension_cnt:int64, cervical_cancer_cnt:int64, breast_cancer_cnt:int64, lung_cancer_cnt:int64, colorectal_cancer_cnt:int64)' > pilot/secret_shares/input/$i/phame_diagnosis_rollup.schema
+  echo '(desc:varchar(42), count:int64, site_id:int32)' > pilot/secret_shares/input/$i/phame_cohort_counts.schema
 done
 
 for host in 1 3; do
-  echo "Row-level schemas for " $host
-  echo '(patid:int32, dx_diabetes:bool, dx_hypertension:bool, dx_cervical_cancer:bool, dx_breast_cancer:bool, dx_lung_cancer:bool, dx_colorectal_cancer:bool)' > pilot/secret-shares/input/$host/phame_diagnosis.schema
-  echo '(patid:int32, age_cat:char(1), gender:char(1), race:char(1), ethnicity:char(1), zip:char(5), payer_primary:char(1), payer_secondary:char(1))' > pilot/secret-shares/input/$host/phame_demographic.schema
+  echo '(patid:int32, dx_diabetes:bool, dx_hypertension:bool, dx_cervical_cancer:bool, dx_breast_cancer:bool, dx_lung_cancer:bool, dx_colorectal_cancer:bool)' > pilot/secret_shares/input/$host/phame_diagnosis.schema
+  echo '(patid:int32, age_cat:char(1), gender:char(1), race:char(1), ethnicity:char(1), zip:char(5), payer_primary:char(1), payer_secondary:char(1))' > pilot/secret_shares/input/$host/phame_demographic.schema
+done
+
+
+### Generate the secret shares
+mkdir -p pilot/secret_shares/output/0
+mkdir pilot/secret_shares/output/1
+mkdir pilot/secret_shares/output/2
+mkdir pilot/secret_shares/output/3
+#clear out old data
+rm pilot/secret_shares/output/*/*
+
+
+for((i = 0; i < $HOST_COUNT; i++)); do
+  ./bin/secret_share_csv pilot/secret_shares/input/$i/phame_cohort_counts.csv   pilot/secret_shares/input/$i/phame_cohort_counts.schema pilot/secret_shares/output/$i/phame_cohort_counts
+  ./bin/secret_share_csv pilot/secret_shares/input/$i/phame_diagnosis_rollup.csv pilot/secret_shares/input/$i/phame_diagnosis_rollup.schema pilot/secret_shares/output/$i/phame_diagnosis_rollup
+done
+
+for host in 1 3 ; do
+  ./bin/secret_share_csv pilot/secret_shares/input/$host/phame_diagnosis.csv pilot/secret_shares/input/$host/phame_diagnosis.schema pilot/secret_shares/output/$host/phame_diagnosis
+  ./bin/secret_share_csv pilot/secret_shares/input/$host/phame_demographic.csv pilot/secret_shares/input/$host/phame_demographic.schema pilot/secret_shares/output/$host/phame_demographic
 done
