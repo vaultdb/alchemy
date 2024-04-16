4efb0211671dd6a4bbf76e16e0e08138f614f90b
Donghyun Sohn <bulelion37@naver.com>
2024-04-16 10:14:42 +0000

    Change

--- src/main/cpp/conf/plans/experiment_7/Bushy_Plan_Enumeration/auto_optimized-q8.sql
+++ src/main/cpp/conf/plans/experiment_7/Bushy_Plan_Enumeration/auto_optimized-q8.sql
@@ -9,14 +9,15 @@ SELECT o_orderkey, o_custkey, o_orderyear::INT AS o_year, NOT (o_orderdate >= da
 FROM orders
 ORDER BY o_custkey, o_orderkey
 -- 4, collation: (0 ASC)
-SELECT l_orderkey, l_suppkey, l_extendedprice * (1.0 - l_discount) AS volume,
-       CASE
-           WHEN p_type = 'LARGE ANODIZED STEEL' AND p_partkey = l_partkey THEN FALSE
-           ELSE TRUE
-           END AS dummy_tag
-FROM lineitem LEFT JOIN part ON p_partkey = l_partkey
+SELECT l_orderkey, l_suppkey, l_partkey, l_extendedprice * (1.0 - l_discount) AS volume
+FROM lineitem
 ORDER BY l_orderkey
 -- 7, collation: (0 ASC)
 SELECT s_suppkey, CASE WHEN n_name = 'KENYA' THEN true ELSE false END AS nation_check
 FROM supplier JOIN nation ON n_nationkey = s_nationkey
 ORDER BY s_suppkey
+-- 10, collation: (0 ASC)
+SELECT p_partkey
+FROM part
+WHERE p_type = 'LARGE ANODIZED STEEL'
+ORDER BY p_partkey
