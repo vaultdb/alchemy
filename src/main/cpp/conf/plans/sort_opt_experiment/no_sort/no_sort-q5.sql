658ec6974db0bbe8ebf3660e7a70a14cae0bc261
donghyun-sohn <bulelion37@naver.com>
2024-06-04 07:01:51 +0000

    Add pre_sorting test

---
+++ src/main/cpp/conf/plans/sort_opt_experiment/no_sort/no_sort-q5.sql
@@ -0,0 +1,16 @@
+-- 0
+SELECT c_custkey, c_nationkey
+FROM customer JOIN nation ON c_nationkey = n_nationkey
+JOIN region ON n_regionkey = r_regionkey
+WHERE r_name = 'EUROPE'
+-- 1
+SELECT o_orderkey, o_custkey, NOT (o_orderdate >= DATE '1993-01-01' AND o_orderdate < DATE '1994-01-01') AS dummy_tag
+FROM orders
+-- 4
+SELECT l_orderkey, l_suppkey, l_extendedprice * (1 - l_discount) AS revenue
+FROM lineitem
+-- 7
+SELECT s_suppkey, s_nationkey, n_name
+FROM supplier JOIN nation ON s_nationkey = n_nationkey
+   JOIN region ON n_regionkey = r_regionkey
+WHERE r_name = 'EUROPE'
