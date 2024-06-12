de14ed28311127d9d8c87fad360f88d9477dc7a9
donghyun-sohn <bulelion37@naver.com>
2024-06-12 21:41:58 +0000

    Modify Q18 plan to not use NLJ

--- src/main/cpp/conf/plans/sort_opt_experiment/no_sort/no_sort-q18.sql
+++ src/main/cpp/conf/plans/sort_opt_experiment/no_sort/no_sort-q18.sql
@@ -2,11 +2,11 @@
 SELECT l_orderkey, l_quantity
 FROM lineitem
 -- 4
-SELECT L2.l_orderkey, L2.l_quantity
-FROM lineitem L2
--- 7
 SELECT o_orderkey, o_custkey, o_totalprice, o_orderdate
 FROM orders
--- 10
+-- 5
 SELECT c_custkey, c_name
 FROM customer
+-- 10
+SELECT L2.l_orderkey, L2.l_quantity
+FROM lineitem L2
--- src/main/cpp/conf/plans/sort_opt_experiment/pre_sort/pre_sort-q18.sql
+++ src/main/cpp/conf/plans/sort_opt_experiment/pre_sort/pre_sort-q18.sql
@@ -2,15 +2,15 @@
 SELECT l_orderkey, l_quantity
 FROM lineitem
 ORDER BY l_orderkey
--- 4, collation: (0 ASC)
-SELECT L2.l_orderkey, L2.l_quantity
-FROM lineitem L2
-ORDER BY L2.l_orderkey
--- 7, collation: (0 ASC)
+-- 4, collation: (1 ASC)
 SELECT o_orderkey, o_custkey, o_totalprice, o_orderdate
 FROM orders
-ORDER BY o_orderkey
--- 10, collation: (0 ASC)
+ORDER BY o_custkey
+-- 5, collation: (0 ASC)
 SELECT c_custkey, c_name
 FROM customer
 ORDER BY c_custkey
+-- 10, collation: (0 ASC)
+SELECT L2.l_orderkey, L2.l_quantity
+FROM lineitem L2
+ORDER BY L2.l_orderkey
