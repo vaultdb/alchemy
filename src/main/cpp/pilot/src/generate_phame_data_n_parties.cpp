e47dac18bc9567a2cf746316320fc0b16708b47b
Jennie Rogers <jennie@northwestern.edu>
2024-07-22 21:56:09 -0500

    re-randomizing synthetic data generator for catalyst

--- src/main/cpp/pilot/src/generate_phame_data_n_parties.cpp
+++ src/main/cpp/pilot/src/generate_phame_data_n_parties.cpp
@@ -43,8 +43,7 @@ int main(int argc, char **argv) {
     string dst_path = argv[1];
     int party_cnt = atoi(argv[2]);
     int tuple_cnt = atoi(argv[3]);
-    //srand (time(nullptr));
-    srand (1);
+    srand (time(nullptr));
 
     dst_path = GeneratePhameDataNParties::getCurrentWorkingDirectory() + "/" + dst_path;
 
