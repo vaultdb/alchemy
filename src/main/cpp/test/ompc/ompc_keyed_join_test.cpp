51ca0285acdb8952ba95fbc8e6aad9e7990893e6
Jennie Rogers <jennie@northwestern.edu>
2024-08-06 13:55:36 -0500

    re-enabled validation

--- src/main/cpp/test/ompc/ompc_keyed_join_test.cpp
+++ src/main/cpp/test/ompc/ompc_keyed_join_test.cpp
@@ -15,7 +15,7 @@ DEFINE_int32(port, 43440, "port for EMP execution");
 DEFINE_string(unioned_db, "tpch_unioned_150", "unioned db name");
 DEFINE_int32(cutoff, 10, "limit clause for queries");
 DEFINE_int32(ctrl_port, 65440, "port for managing EMP control flow by passing public values");
-DEFINE_bool(validation, false, "run reveal for validation, turn this off for benchmarking experiments (default true)");
+DEFINE_bool(validation, true, "run reveal for validation, turn this off for benchmarking experiments (default true)");
 DEFINE_string(filter, "*", "run only the tests passing this filter");
 DEFINE_string(storage, "wire_packed", "storage model for columns (column or wire_packed)");
 DEFINE_string(empty_db, "tpch_empty", "empty db name for schemas");
