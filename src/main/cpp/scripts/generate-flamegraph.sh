190a18f5a2d33129b306fbfabd9d9b4d681d2507
Jennie Rogers <jennie@northwestern.edu>
2024-08-07 14:13:38 -0500

    working on flamegraph scripts

--- src/main/cpp/scripts/generate-flamegraph.sh
+++ src/main/cpp/scripts/generate-flamegraph.sh
@@ -2,7 +2,7 @@
 
 if [ "$#" -ne 1 ]; then
   #example: bash scripts/generate-flamegraph.sh bin/ompc_block_nested_loop_join_test
-    echo "usage: scripts/generate-flamegraph.sh <test file>"
+    echo "usage: scripts/generate-flamegraph.sh <test>"
     exit
 fi
 
@@ -13,12 +13,24 @@ TEST=$1
 # cd _deps && git clone https://github.com/brendangregg/FlameGraph
 FLAMEGRAPH="_deps/FlameGraph"
 
-$PERF_EXE record -F 100 -a --call-graph dwarf -m256M $TEST --party=1 & $TEST --party=2 & $TEST --party=3 & $TEST --party=10086
+$PERF_EXE record -F 100 -a --call-graph dwarf -m256M $TEST --party=1 &
+./$TEST --party=2 &
+./$TEST --party=3 &
+./$TEST --party=10086
 wait
 sleep 1
 sync
-$PERF_EXE script -f | ${FLAMEGRAPH}/stackcollapse-perf.pl > out.folded && ${FLAMEGRAPH}/flamegraph.pl out.folded > `basename ${TEST}`.svg wait
-########################### # try pprof here ########################### $exe --party=2 &
+
+$PERF_EXE script -f | ${FLAMEGRAPH}/stackcollapse-perf.pl > out.folded && ${FLAMEGRAPH}/flamegraph.pl out.folded > `basename ${TEST}`.svg
+
+wait
+
+########################### # try pprof here ###########################
+$TEST --party=2 &
+./$TEST --party=3 &
+./$TEST --party=10086
+
 env CPUPROFILE=`basename ${TEST}.prof` $TEST --party=1
 wait
-/usr/local/bin/pprof --pdf $TEST `basename ${TEST}.prof` &> `basename ${TEST}.pdf` echo "script done"
+/usr/local/bin/pprof --pdf $TEST `basename ${TEST}.prof` &> `basename ${TEST}.pdf`
+echo "script done"
