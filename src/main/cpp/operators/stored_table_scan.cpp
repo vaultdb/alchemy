9e60c90e7c7aa378d3d43f4ec18b5fe412cab6a7
xilinggrantli <xiling.li@northwestern.edu>
2024-09-25 18:26:40 -0500

    finish data loading for column store; working on baseline with buffer pool

--- src/main/cpp/operators/stored_table_scan.cpp
+++ src/main/cpp/operators/stored_table_scan.cpp
@@ -280,92 +280,105 @@ QueryTable<B> *StoredTableScan<B>::readSecretSharedStoredTable(string table_name
     TableMetadata md = conf.table_metadata_.at(table_name);
 
     // else, first construct dst schema from projection
-    auto dst_schema = OperatorUtilities::deriveSchema(md.schema_, col_ordinals);
-    auto dst_collation = OperatorUtilities::deriveCollation(md.collation_, col_ordinals);
+    QuerySchema dst_schema = OperatorUtilities::deriveSchema(md.schema_, col_ordinals);
+    SortDefinition dst_collation = OperatorUtilities::deriveCollation(md.collation_, col_ordinals);
 
     QuerySchema secure_schema = QuerySchema::toSecure(md.schema_);
     QuerySchema plain_schema = QuerySchema::toPlain(md.schema_);
 
-    size_t src_tuple_cnt = md.tuple_cnt_;
-    size_t tuple_cnt = (limit == -1 || limit > src_tuple_cnt) ? src_tuple_cnt : limit;
+    int src_tuple_cnt = md.tuple_cnt_;
+    int tuple_cnt = (limit == -1 || limit > src_tuple_cnt) ? src_tuple_cnt : limit;
 
-    bool *dst_bools;
-    size_t dst_bit_cnt = tuple_cnt * dst_schema.size();
+    int dst_bit_cnt = tuple_cnt * dst_schema.size();
+    Integer dst_int(dst_bit_cnt, 0L, emp::PUBLIC);
 
-    if(!conf.inputParty()) {
-        string secret_shares_file = SystemConfiguration::getInstance().stored_db_path_ + "/" + table_name + "." +
-                                    std::to_string(SystemConfiguration::getInstance().party_);
+    int currernt_emp_bit_size_on_disk = (conf.party_ == 1 ? empBitSizesInPhysicalBytes::evaluator_disk_size_ : (conf.party_ == 10086 ? 1 : empBitSizesInPhysicalBytes::garbler_disk_size_));
 
-        // deduce row count
-        size_t src_byte_cnt = std::filesystem::file_size(secret_shares_file);
-        size_t src_bit_cnt = src_byte_cnt * 8;
+    map<int, int64_t> ordinal_offsets;
+    int64_t array_byte_cnt = 0L;
+    ordinal_offsets[0] = 0;
+    for (int i = 1; i < md.schema_.getFieldCount(); ++i) {
+        array_byte_cnt += md.schema_.getField(i - 1).size() * src_tuple_cnt * currernt_emp_bit_size_on_disk;
+        ordinal_offsets[i] = array_byte_cnt;
+    }
+    array_byte_cnt += md.schema_.getField(md.schema_.getFieldCount() - 1).size() * src_tuple_cnt * currernt_emp_bit_size_on_disk;
+    ordinal_offsets[-1] = array_byte_cnt;
 
+    auto *auth_shares = new AuthShare<emp::N>[dst_bit_cnt];
+    bool *masked_values = new bool[dst_bit_cnt]();
 
-        map<int, int64_t> ordinal_offsets;
-        int64_t array_byte_cnt = 0L;
-        ordinal_offsets[0] = 0;
-        for (int i = 1; i < md.schema_.getFieldCount(); ++i) {
-            array_byte_cnt += (md.schema_.getField(i - 1).size() * src_tuple_cnt)/8;
-            ordinal_offsets[i] = array_byte_cnt;
-        }
-        array_byte_cnt += (md.schema_.getField(md.schema_.getFieldCount() - 1).size() * src_tuple_cnt)/8;
-        ordinal_offsets[-1] = array_byte_cnt;
-        array_byte_cnt += md.schema_.getField(-1).size() * src_tuple_cnt;
+    string secret_shares_file = SystemConfiguration::getInstance().stored_db_path_ + "/" + table_name + "." + std::to_string(conf.party_);
 
-        // convert serialized representation from byte-aligned to bit-by-bit for EMP
-        dst_bools = new bool[dst_bit_cnt]; // dst_bit_alloc
-        bool *dst_cursor = dst_bools;
+    FILE*  fp = fopen(secret_shares_file.c_str(), "rb");
+    int file_offset = 0;
+    int bit_cursor = 0;
 
-        assert(src_bit_cnt % plain_schema.size() == 0);
-        FILE *fp = fopen(secret_shares_file.c_str(), "rb");
+    for(auto src_ordinal : col_ordinals) {
+        auto secure_field = secure_schema.getField(src_ordinal);
+        int read_size_bits = tuple_cnt * secure_field.size();
 
-        for (auto src_ordinal: col_ordinals) {
-            int read_size_bits = tuple_cnt * secure_schema.getField(src_ordinal).size();
-            fseek(fp, ordinal_offsets[src_ordinal], SEEK_SET); // read read_offset bytes from beginning of file
-            if (plain_schema.getField(src_ordinal).getType() != FieldType::BOOL) {
-                vector<int8_t> tmp_read(read_size_bits / 8);
-                fread(tmp_read.data(), 1, read_size_bits / 8, fp);
-                emp::to_bool<int8_t>(dst_cursor, tmp_read.data(), read_size_bits, false);
-                dst_cursor += read_size_bits;
-            } else { // just copy it in
-                fread(dst_cursor, 1, read_size_bits, fp);
-                for(int j = 0; j < tuple_cnt; ++j) {
-                    *dst_cursor = ((*dst_cursor & 1) != 0); // (bool)
-                    ++dst_cursor;
-                }
+        file_offset = ordinal_offsets[src_ordinal];
+        fseek(fp, file_offset, SEEK_SET);
+
+        for(int j = 0; j < read_size_bits; ++j) {
+            AuthShare<emp::N> cur_auth_share;
+
+            if(!conf.inputParty()) {
+                fread((int8_t *) &cur_auth_share.mac, 1, emp::N * sizeof(emp::block), fp);
+                fread((int8_t *) &cur_auth_share.key, 1, emp::N * sizeof(emp::block), fp);
             }
-        }
 
-        // read dummy tag unconditionally
-        fseek(fp, ordinal_offsets[-1], SEEK_SET);
-        fread(dst_cursor, 1, tuple_cnt, fp); // 1 byte / row
-        for(int j = 0; j < tuple_cnt; ++j) {
-            *dst_cursor = ((*dst_cursor & 1) != 0); // (bool)
-            ++dst_cursor;
-        }
+            int8_t cur_lambda = 0;
+            fread((int8_t *) &cur_lambda, 1, 1, fp);
+            cur_auth_share.lambda = ((cur_lambda & 1) != 0);
 
-        fclose(fp);
+            auth_shares[bit_cursor] = cur_auth_share;
+
+            if(conf.party_ == 1) {
+                int8_t cur_masked_value = 0;
+                fread((int8_t *) &cur_masked_value, 1, 1, fp);
+                masked_values[bit_cursor] = ((cur_masked_value & 1) != 0);
+            }
+
+            ++bit_cursor;
+        }
     }
 
+    // read dummy tag
+    file_offset = ordinal_offsets[-1];
+    fseek(fp, file_offset, SEEK_SET);
 
-    Integer dst(dst_bit_cnt, 0L, emp::PUBLIC);
+    for(int i = 0; i < tuple_cnt; ++i) {
+        AuthShare<emp::N> cur_auth_share;
 
-    int party = SystemConfiguration::getInstance().party_;
-    EmpManager *manager = SystemConfiguration::getInstance().emp_manager_;
-    bool *to_send = (party == 1) ? dst_bools : nullptr;
-    manager->feed(dst.bits.data(), 1, to_send, dst_bit_cnt);
+        if(!conf.inputParty()) {
+            fread((int8_t *) &cur_auth_share.mac, 1, emp::N * sizeof(emp::block), fp);
+            fread((int8_t *) &cur_auth_share.key, 1, emp::N * sizeof(emp::block), fp);
+        }
 
-    for(int i = 2; i <= N; ++i) {
-        Integer tmp(dst_bit_cnt, 0L, emp::PUBLIC);
-        to_send = (party == i) ? dst_bools : nullptr;
-        manager->feed(tmp.bits.data(), i, to_send, dst_bit_cnt);
-        dst = dst ^ tmp;
+        int8_t cur_lambda = 0;
+        fread((int8_t *) &cur_lambda, 1, 1, fp);
+        cur_auth_share.lambda = ((cur_lambda & 1) != 0);
+
+        auth_shares[dst_bit_cnt - tuple_cnt + i] = cur_auth_share;
+
+        if(conf.party_ == 1) {
+            int8_t cur_masked_value = 0;
+            fread((int8_t *) &cur_masked_value, 1, 1, fp);
+            masked_values[dst_bit_cnt - tuple_cnt + i] = ((cur_masked_value & 1) != 0);
+        }
     }
 
-    SecureTable *shared_table = QueryTable<Bit>::deserialize(dst_schema, dst.bits);
-    shared_table->order_by_ = md.collation_;
+    fclose(fp);
 
-    if(!conf.inputParty()) delete [] dst_bools;
+    ((OutsourcedMpcManager *) conf.emp_manager_)->protocol_->regen_label(dst_int.bits.data(), masked_values, auth_shares, dst_bit_cnt);
+
+    delete [] auth_shares;
+    delete [] masked_values;
+
+    SecureTable *shared_table = QueryTable<Bit>::deserialize(dst_schema, dst_int.bits);
+    shared_table->order_by_ = dst_collation;
+
     return (QueryTable<B> *) shared_table;
 }
 
@@ -437,8 +450,8 @@ QueryTable<B> *StoredTableScan<B>::readSecretSharedStoredTable(string table_name
         AuthShare<emp::N> cur_auth_share;
 
         if(!conf.inputParty()) {
-            fread((int8_t *) &cur_auth_share.mac, sizeof(emp::block), emp::N, fp);
-            fread((int8_t *) &cur_auth_share.key, sizeof(emp::block), emp::N, fp);
+            fread((int8_t *) &cur_auth_share.mac, 1, emp::N * sizeof(emp::block), fp);
+            fread((int8_t *) &cur_auth_share.key, 1, emp::N * sizeof(emp::block), fp);
         }
 
         int8_t cur_lambda = 0;
