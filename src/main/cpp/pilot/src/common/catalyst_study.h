41a30120f2c659f93ddc92043d3b8b41c8af863a
Jennie Rogers <jennie@northwestern.edu>
2024-10-04 17:12:24 -0500

    adding more debugs to pilot code

--- src/main/cpp/pilot/src/common/catalyst_study.h
+++ src/main/cpp/pilot/src/common/catalyst_study.h
@@ -32,6 +32,37 @@ namespace catalyst {
         // all paths  relative to $VAULTDB_HOME/src/main/cpp
         string secret_shares_path_; // root directory for secret shares,
         string dst_path_; // where to write secret shares of query output
+
+        string toString() const {
+            stringstream ss;
+
+            ss << "Study: " << study_name_ << endl;
+            ss << "Min cell count: " << min_cell_cnt_ << endl;
+            ss << "Secret shares path: " << secret_shares_path_ << endl;
+            ss << "Destination path: " << dst_path_ << endl;
+            ss << "Input tables: " << endl;
+            for(auto t : input_tables_) {
+                ss << "Table: " << t.name_ << endl;
+                ss << "     Schema: " << t.schema_ << endl;
+                ss << "     Data contributors: ";
+                for(auto c : t.data_contributors_) {
+                    ss << c << " ";
+                }
+                ss << endl;
+            }
+
+            ss << "Queries: " << endl;
+            for(auto q : queries_) {
+                ss << "Query: " << q.first << endl;
+                ss << "     Plan file: " << q.second.plan_file_ << endl;
+                ss << "     Count columns: ";
+                for(auto c : q.second.count_cols_) {
+                    ss << c << " ";
+                }
+                ss << endl;
+            }
+            return ss.str();
+        }
     };
 }
 #endif
