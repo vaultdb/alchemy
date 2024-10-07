51f92636cdef3414620f443101ccf1bcffa77483
VaultDB <vaultdb@vaultdb.org>
2024-10-07 21:13:20 +0000

    revising script for current pilot config

--- src/main/cpp/scripts/reconnect-pilot-bob.sh
+++ src/main/cpp/scripts/reconnect-pilot-bob.sh
@@ -5,4 +5,7 @@ kill $SSH_PID
 
 sleep 1
 
-ssh -f -i /home/vaultdb/.ssh/catalyst -N -p 3333 -L 4444:127.0.0.1:4444  vaultdb@alice
+#ssh -f -i /home/vaultdb/.ssh/catalyst -N -p 3333 -L 4444:127.0.0.1:4444  vaultdb@alice
+#
+# add a hop for alliance
+ssh -f -N -p 3333 -J 10.146.0.4 -L 4444:127.0.0.1:4444  vaultdb@165.124.123.122
