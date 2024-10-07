28f3d1671e89300e5ffe1cad511d7fd5bcfb7879
VaultDB <vaultdb@vaultdb.org>
2024-10-07 22:54:21 +0000

    fixed a bug in pilot connnection scripts

--- src/main/cpp/scripts/reconnect-pilot-bob.sh
+++ src/main/cpp/scripts/reconnect-pilot-bob.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-SSH_PID=$(ps ax | grep 'ssh -f' | head  -n 1  | sed 's/\ .*$//g')
+SSH_PID=$(ps -ef | grep 'ssh -f' | grep -v grep | awk '{print $2}')
 kill $SSH_PID
 
 sleep 1
@@ -8,4 +8,4 @@ sleep 1
 #ssh -f -i /home/vaultdb/.ssh/catalyst -N -p 3333 -L 4444:127.0.0.1:4444  vaultdb@alice
 #
 # add a hop for alliance
-ssh -f -N -p 3333 -J 10.146.0.4 -L 4444:127.0.0.1:4444  vaultdb@165.124.123.122
+ssh -f -N -i /home/vaultdb/.ssh/catalyst -p 3333 -J 10.146.0.4 -L 4444:127.0.0.1:4444  vaultdb@165.124.123.122
