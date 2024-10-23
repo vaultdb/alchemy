eba9fc00be3cad7e7950ca584b04c318470cda5f
Jennie Rogers <jennie@northwestern.edu>
2024-10-22 20:03:39 -0500

    reverting to script with just one hop for pilot

--- src/main/cpp/scripts/reconnect-pilot-bob.sh
+++ src/main/cpp/scripts/reconnect-pilot-bob.sh
@@ -5,7 +5,7 @@ kill $SSH_PID
 
 sleep 1
 
-#ssh -f -i /home/vaultdb/.ssh/catalyst -N -p 3333 -L 4444:127.0.0.1:4444  vaultdb@alice
+ssh -f -i /home/vaultdb/.ssh/catalyst -N -p 3333 -L 4444:127.0.0.1:4444  vaultdb@alice
 #
 # add a hop for alliance
-ssh -f -N -i /home/vaultdb/.ssh/catalyst -p 3333 -J 10.146.0.6 -L 4444:127.0.0.1:4444  vaultdb@165.124.123.122
+#ssh -f -N -i /home/vaultdb/.ssh/catalyst -p 3333 -J 10.146.0.6 -L 4444:127.0.0.1:4444  vaultdb@165.124.123.122
